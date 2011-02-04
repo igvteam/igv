0416821375b603db7331a1c2f798d226f0e74124
Matt Damon <damon.may@gmail.com>
2011-02-04 18:12:13 +0000

    Fixing PackedFeaturesSpliceJunctions so that it resolves conflicts between features within each strand, then merges the two strands
    git-svn-id: https://igv.googlecode.com/svn/trunk@225 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/track/PackedFeaturesSpliceJunctions.java
+++ src/org/broad/igv/track/PackedFeaturesSpliceJunctions.java
@@ -22,6 +22,7 @@ package org.broad.igv.track;
 import org.apache.log4j.Logger;
 import org.broad.igv.feature.BasicFeature;
 import org.broad.igv.feature.IGVFeature;
+import org.broad.igv.feature.Strand;
 import org.broad.igv.renderer.SpliceJunctionRenderer;
 import org.broad.igv.ui.util.MessageUtils;
 import org.broad.tribble.Feature;
@@ -35,18 +36,16 @@ import java.util.*;
  * This class is a subclass of PackedFeatures that is for display of splice junctions. It overrides some
  * methods in order to deviate from superclass in two ways:
  * 1.  Features are allowed to be on the same line if flanking regions overlap
- * 2.  Features are ordered from top to bottom in ascending order of read depth
+ * 2.  Overlapping features are allowed to be on the same line if they are from different strands
+ * 3.  Features are ordered from top to bottom in ascending order of read depth
  * The goal is for the dominant isoform to appear on the top row.
  *
  * I think I've got the 90% case covered, but there's quite a bit of ambiguity here.  Some items for future work:
  * -The feature ordering will probably fail in certain conditions, e.g., an exon removal in which the situation
  * where the exon is present has more coverage in the first junction, but the absent-exon condition has more
  * coverage overall.  These are kind of degenerate cases, so only worth handling if someone complains.
- * -+ and - strand features occurring simultaneously.  Currently, not attempt is made to separate those features.
- * This could be done by dividing them up first, doing the packFeatures thing, and recombining.  This should probably
- * be done, but it's work.
  */
-public class PackedFeaturesSpliceJunctions<T extends Feature> extends PackedFeatures{
+public class PackedFeaturesSpliceJunctions<T extends Feature> extends PackedFeatures {
     private static Logger log = Logger.getLogger(PackedFeaturesSpliceJunctions.class);
 
     PackedFeaturesSpliceJunctions(String chr, int start, int end, Iterator<T> iter, String trackName) {
@@ -80,12 +79,61 @@ public class PackedFeaturesSpliceJunctions<T extends Feature> extends PackedFeat
 
     /**
      * Allocates each alignment to the rows such that there is no overlap. For splice junctions, priority queues
-     * are ordered by feature score (read depth).  For the superclass, this is done by length
+     * are ordered by feature score (read depth).  For the superclass, this is done by length.
+     * Since splice junctions only interfere with each other within a strand, break up the iterator into one
+     * iterator per strand, farm out the work per strand, and reintegrate.
+     *
+     * This seems nice and clean, but it's actually not that efficient, since we're handling all of one strand
+     * and then all of the other -- we're essentially buffering all the second strand's features until we get
+     * to them
      *
      * @param iter TabixLineReader wrapping the collection of alignments
      */
     List<FeatureRow> packFeatures(Iterator iter) {
+        IteratorSplitterByCharge iterSplitter = new IteratorSplitterByCharge(iter);
 
+        List<FeatureRow> posRows = packFeaturesOneStrand(iterSplitter.getPosIter());
+        List<FeatureRow> negativeRows = packFeaturesOneStrand(iterSplitter.getNegIter());
+
+        Comparator startComparator = new Comparator<Feature>() {
+            public int compare(Feature row1, Feature row2) {
+                return row1.getStart() - row2.getStart();
+            }
+        };
+
+        int numRows = Math.max(posRows.size(), negativeRows.size());
+        List<FeatureRow> result = new ArrayList<FeatureRow>(numRows);
+        features.clear();
+        for (int i=0; i<numRows; i++)
+        {
+            List<Feature> posAndNegFeatures = new ArrayList<Feature>();
+            if (negativeRows.size() > i)
+                posAndNegFeatures.addAll(negativeRows.get(i).getFeatures());
+            if (posRows.size() > i)
+                posAndNegFeatures.addAll(posRows.get(i).getFeatures());
+
+            if (!posAndNegFeatures.isEmpty())
+            {
+                Collections.sort(posAndNegFeatures, startComparator);
+                FeatureRow resultRow = new FeatureRow();
+                for (Feature feature : posAndNegFeatures)
+                    resultRow.addFeature(feature);
+                result.add(resultRow);
+                features.addAll(posAndNegFeatures);
+            }
+        }
+        Collections.sort(features, startComparator);
+        return result;
+    }
+
+
+    /**
+     * This does the real work of packing features, pretty much the same way the superclass does, except
+     * that features can overlap in their flanking regions and they're ordered among the rows by score
+     * @param iter
+     * @return
+     */
+    List<FeatureRow> packFeaturesOneStrand(Iterator iter) {
         List<FeatureRow> rows = new ArrayList(10);
         if (iter == null || !iter.hasNext()) {
             return rows;
@@ -187,4 +235,94 @@ public class PackedFeaturesSpliceJunctions<T extends Feature> extends PackedFeat
 
         return rows;
     }
+
+    /**
+     * Takes in an iterator of Features and creates two new iterators, one that gives the neg-strand features
+     * and one that gives the pos-strand features.  Does this by buffering features of the opposite strand
+     * when a feature of a particular strand is requested.
+     */
+    protected class IteratorSplitterByCharge {
+        Iterator origIter;
+
+        List<Feature> posBuffer = new ArrayList<Feature>();
+        List<Feature> negBuffer = new ArrayList<Feature>();
+
+        PerStrandIter posIter;
+        PerStrandIter negIter;
+
+        public IteratorSplitterByCharge(Iterator origIter) {
+            this.origIter = origIter;
+
+            posIter = new PerStrandIter(posBuffer, negBuffer, Strand.POSITIVE);
+            negIter = new PerStrandIter(negBuffer, posBuffer, Strand.NEGATIVE);
+        }
+
+        public PerStrandIter getPosIter() {
+            return posIter;
+        }
+
+        public PerStrandIter getNegIter() {
+            return negIter;
+        }
+
+        /**
+         * An iterator of Features for a single strand. Polls the original iterator for new features and
+         * either returns them or buffers them in the other strand's buffer, accordingly
+         */
+        protected class PerStrandIter implements Iterator<Feature> {
+            List<Feature> buffer;
+            List<Feature> otherBuffer;
+            Strand strand;
+
+
+            public PerStrandIter(List<Feature> buffer, List<Feature> otherBuffer, Strand strand) {
+                this.buffer = buffer;
+                this.otherBuffer = otherBuffer;
+                this.strand = strand;
+            }
+
+            /**
+             * Burn through the original iterator until we either hit the end or find a feature of the
+             * appropriate strand, buffering the other strand features as we go
+             * @return
+             */
+            public boolean hasNext() {
+                while (buffer.isEmpty() && origIter.hasNext())
+                {
+                    BasicFeature feature = (BasicFeature) origIter.next();
+                    if (feature.getStrand() == strand)
+                        buffer.add(feature);
+                    else
+                        otherBuffer.add(feature);
+                }
+                return !buffer.isEmpty();
+            }
+
+            /**
+             * Burn through the original iterator until we either hit the end or find a feature of the
+             * appropriate strand, buffering the other strand features as we go
+             * @return
+             */
+            public Feature next() {
+                while (buffer.isEmpty() && origIter.hasNext())
+                {
+                    BasicFeature feature = (BasicFeature) origIter.next();
+                    if (feature.getStrand() == strand)
+                        buffer.add(feature);
+                    else
+                        otherBuffer.add(feature);
+                }
+                if (buffer.isEmpty())
+                    return null;
+                Feature result = buffer.get(0);
+                buffer.remove(0);
+                return result;
+            }
+
+            public void remove() {
+                //not implemented
+            }
+        }
+    }
+
 }
