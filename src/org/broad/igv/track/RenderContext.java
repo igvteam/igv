499889755721e36523b6fedcf8eb85a33ff25a2c
James Robinson <jrobinso@broadinstitute.org>
2011-04-24 01:53:24 +0000

    Added background color to render context
    git-svn-id: https://igv.googlecode.com/svn/trunk@469 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/track/RenderContext.java
+++ src/org/broad/igv/track/RenderContext.java
@@ -63,6 +63,11 @@ public class RenderContext {
         return g;
     }
 
+
+    public Color getBackgroundColor() {
+        return panel.getBackground();
+    }
+
     public String getChr() {
         return referenceFrame.getChrName();
     }
