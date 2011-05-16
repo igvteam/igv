45a9a4f8d30517f324a539914fe37f1b12dfb2e1
James Robinson <jrobinso@broadinstitute.org>
2011-05-16 02:37:27 +0000

    Removed kaplan-meier curve for 2.0
    git-svn-id: https://igv.googlecode.com/svn/trunk@585 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/track/TrackMenuUtils.java
+++ src/org/broad/igv/track/TrackMenuUtils.java
@@ -306,7 +306,7 @@ public class TrackMenuUtils {
         menu.add(getShowDataRangeItem(tracks));
 
 
-        menu.add(getChangeKMPlotItem(tracks));
+        //menu.add(getChangeKMPlotItem(tracks));
 
     }
 
