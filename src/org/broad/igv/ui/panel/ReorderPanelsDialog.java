7fd50c5dcd45e9440c0234a4899568260c5260e5
James Robinson <jrobinso@broadinstitute.org>
2011-05-16 04:33:20 +0000

    Added classes to permit reordering of panels -- partially complete
    git-svn-id: https://igv.googlecode.com/svn/trunk@587 17392f64-ead8-4cea-ae29-09b3ab513800

---
+++ src/org/broad/igv/ui/panel/ReorderPanelsDialog.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2007-2011 by The Broad Institute, Inc. and the Massachusetts Institute of
+ * Technology.  All Rights Reserved.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
+ * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
+ * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
+ * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
+ * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
+ * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
+ * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
+ * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
+ */
+
+/*
+ * Created by JFormDesigner on Mon May 16 00:05:02 EDT 2011
+ */
+
+package org.broad.igv.ui.panel;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+import org.broad.igv.ui.IGV;
+import org.broad.igv.ui.util.*;
+
+/**
+ * @author Stan Diamond
+ */
+public class ReorderPanelsDialog extends JDialog {
+
+    private boolean canceled = false;
+
+    public ReorderPanelsDialog(Frame owner) {
+        super(owner);
+        initComponents();
+    }
+
+    public ReorderPanelsDialog(Dialog owner) {
+        super(owner);
+        initComponents();
+    }
+
+    private void okButtonActionPerformed(ActionEvent e) {
+        setVisible(false);
+    }
+
+    private void cancelButtonActionPerformed(ActionEvent e) {
+        canceled = true;
+        setVisible(false);
+    }
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner non-commercial license
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        scrollPane1 = new JScrollPane();
+        list = new ReorderableJList();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+        cancelButton = new JButton();
+
+        //======== this ========
+        setTitle("Reorder Panels");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new GridLayout());
+
+                //======== scrollPane1 ========
+                {
+                    scrollPane1.setViewportView(list);
+                }
+                contentPanel.add(scrollPane1);
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
+                buttonBar.setLayout(new GridBagLayout());
+                ((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 85, 80};
+                ((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0, 0.0};
+
+                //---- okButton ----
+                okButton.setText("OK");
+                okButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        okButtonActionPerformed(e);
+                    }
+                });
+                buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- cancelButton ----
+                cancelButton.setText("Cancel");
+                cancelButton.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        cancelButtonActionPerformed(e);
+                    }
+                });
+                buttonBar.add(cancelButton, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner non-commercial license
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JScrollPane scrollPane1;
+    private ReorderableJList list;
+    private JPanel buttonBar;
+    private JButton okButton;
+    private JButton cancelButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
---
+++ src/org/broad/igv/ui/util/ReorderableJList.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) 2007-2011 by The Broad Institute, Inc. and the Massachusetts Institute of
+ * Technology.  All Rights Reserved.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
+ * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
+ * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
+ * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
+ * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
+ * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
+ * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
+ * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
+ */
+
+package org.broad.igv.ui.util;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragGestureRecognizer;
+import java.awt.dnd.DragSource;
+import java.awt.dnd.DragSourceDragEvent;
+import java.awt.dnd.DragSourceDropEvent;
+import java.awt.dnd.DragSourceEvent;
+import java.awt.dnd.DragSourceListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.swing.*;
+
+public class ReorderableJList<T> extends JList {
+    DefaultListModel model;
+
+    public ReorderableJList() {
+
+        setTransferHandler(new MyListDropHandler());
+        new MyDragListener(this);
+    }
+
+    private void setElements(List<T> elements) {
+        model = new DefaultListModel();
+        setModel(model);
+        setDragEnabled(true);
+        setDropMode(DropMode.INSERT);
+
+        for(T obj : elements) {
+            model.addElement(obj);
+        }
+    }
+
+    public List<T> getElements() {
+        List<T> elementList = new ArrayList<T>();
+        Enumeration en = model.elements();
+        while(en.hasMoreElements()) {
+            elementList.add((T) en.nextElement());
+        }
+        return elementList;
+    }
+
+    class MyDragListener implements DragSourceListener, DragGestureListener {
+        ReorderableJList list;
+
+        DragSource ds = new DragSource();
+
+        public MyDragListener(ReorderableJList list) {
+            this.list = list;
+            DragGestureRecognizer dgr = ds.createDefaultDragGestureRecognizer(list,
+                    DnDConstants.ACTION_MOVE, this);
+
+        }
+
+        public void dragGestureRecognized(DragGestureEvent dge) {
+            StringSelection transferable = new StringSelection(Integer.toString(list.getSelectedIndex()));
+            ds.startDrag(dge, DragSource.DefaultCopyDrop, transferable, this);
+        }
+
+        public void dragEnter(DragSourceDragEvent dsde) {
+        }
+
+        public void dragExit(DragSourceEvent dse) {
+        }
+
+        public void dragOver(DragSourceDragEvent dsde) {
+        }
+
+        public void dragDropEnd(DragSourceDropEvent dsde) {
+            if (dsde.getDropSuccess()) {
+                //System.out.println("Succeeded");
+            } else {
+                //System.out.println("Failed");
+            }
+        }
+
+        public void dropActionChanged(DragSourceDragEvent dsde) {
+        }
+    }
+
+    class MyListDropHandler extends TransferHandler {
+
+        public boolean canImport(TransferHandler.TransferSupport support) {
+            if (!support.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                return false;
+            }
+            JList.DropLocation dl = (JList.DropLocation) support.getDropLocation();
+            if (dl.getIndex() == -1) {
+                return false;
+            } else {
+                return true;
+
+            }
+        }
+
+        public boolean importData(TransferHandler.TransferSupport support) {
+            if (!canImport(support)) {
+                return false;
+            }
+
+            Transferable transferable = support.getTransferable();
+            String indexString;
+            try {
+                indexString = (String) transferable.getTransferData(DataFlavor.stringFlavor);
+            } catch (Exception e) {
+                return false;
+            }
+
+            int fromIndex = Integer.parseInt(indexString);
+            JList.DropLocation dl = (JList.DropLocation) support.getDropLocation();
+            int toIndex = dl.getIndex();
+
+            if(fromIndex < toIndex) {
+                toIndex--;
+            }
+
+            Object obj = model.remove(fromIndex);
+            model.add(toIndex, obj);
+            invalidate();
+
+            return true;
+        }
+    }
+
+
+    // Example
+
+
+    public static void main(String[] a) {
+        List<String> list = Arrays.asList("a", "b", "c");
+        JDialog f = new JDialog();
+        f.setModal(true);
+        final ReorderableJList<String> roList = new ReorderableJList<String>();
+        roList.setElements(list);
+        f.add(new JScrollPane(roList));
+        f.setSize(300, 300);
+        f.setVisible(true);
+
+        for(String s : roList.getElements()) {
+            System.out.println(s);
+        }
+
+    }
+
+
+
+}
