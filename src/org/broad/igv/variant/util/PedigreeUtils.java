ba85209178df684b34c629a5308df4733b3819a9
James Robinson <jrobinso@broadinstitute.org>
2011-08-02 15:27:10 +0000

    Major VCF refactoring.   Introduced interfaces for Variant, Genotype, and Allele.  VCFTrack and related classes are renamed to VariantTrack, etc, and now have no direct dependencies on VCF.
    git-svn-id: https://igv.googlecode.com/svn/trunk@890 17392f64-ead8-4cea-ae29-09b3ab513800

---
+++ src/org/broad/igv/variant/util/PedigreeUtils.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
+ * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
+ * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
+ * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
+ * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
+ * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
+ * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
+ * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
+ */
+
+package org.broad.igv.variant.util;
+
+import org.broad.igv.track.AttributeManager;
+import org.broad.igv.ui.IGV;
+import org.broad.igv.util.ParsingUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * @author jrobinso
+ * @date Apr 25, 2011
+ */
+public class PedigreeUtils {
+
+
+    /**
+     * Parses a pedigree file containing trios of child, father, mother.
+     *
+     * @param path
+     * @throws IOException
+     */
+
+    public static void parseTrioFile(String path) throws IOException {
+
+        BufferedReader reader = null;
+
+
+        try {
+            reader = ParsingUtils.openBufferedReader(path);
+            String nextLine;
+            int familyNumber = 1;
+            while ((nextLine = reader.readLine()) != null) {
+                String[] tokens = nextLine.split("\\s+");
+                if (tokens.length == 3) {
+                    String family = String.valueOf(familyNumber);
+                    for (String member : tokens) {
+                        AttributeManager.getInstance().addAttribute(member, "Family", family);
+                    }
+                    String child = tokens[0];
+                    String father = tokens[1];
+                    String mother = tokens[2];
+                    AttributeManager.getInstance().addAttribute(child, "Member", "child");
+                    AttributeManager.getInstance().addAttribute(child, "Relation", "child");
+                    AttributeManager.getInstance().addAttribute(father, "Member", "father");
+                    AttributeManager.getInstance().addAttribute(father, "Relation", "parent");
+                    AttributeManager.getInstance().addAttribute(mother, "Member", "mother");
+                    AttributeManager.getInstance().addAttribute(mother, "Relation", "parent");
+                    familyNumber++;
+                }
+            }
+            IGV.getInstance().getTrackManager().refreshData();
+        } finally {
+            if (reader != null) reader.close();
+        }
+
+    }
+}
---
+++ src/org/broad/igv/variant/util/VCFtoBed.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
+ * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
+ * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
+ * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
+ * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
+ * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
+ * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
+ * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
+ */
+
+package org.broad.igv.variant.util;
+
+import org.broad.igv.feature.tribble.CodecFactory;
+import org.broad.tribble.FeatureCodec;
+import org.broad.tribble.source.BasicFeatureSource;
+import org.broadinstitute.sting.utils.codecs.vcf.VCFCodec;
+import org.broadinstitute.sting.utils.variantcontext.VariantContext;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+/**
+ * Utility class to convert a VCF to a BED file.  Genotypes if any are lost in this converstion
+ *
+ * @author jrobinso
+ * @date Jan 22, 2011
+ */
+public class VCFtoBed {
+
+
+    public static void main(String[] args) throws IOException {
+        convert(args[0], args[1]);
+    }
+
+    public static void convert(String vcfFile, String bedFile) throws IOException {
+
+        BasicFeatureSource basicReader = null;
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(new BufferedWriter(new FileWriter(bedFile)));
+            FeatureCodec codec = CodecFactory.getCodec(vcfFile);
+            boolean isVCF = codec.getClass().isAssignableFrom(VCFCodec.class);
+            basicReader = BasicFeatureSource.getFeatureSource(vcfFile, codec, true);
+
+            Iterator<VariantContext> iter = basicReader.iterator();
+
+            while (iter.hasNext()) {
+
+                VariantContext vc = iter.next();
+                String chr = vc.getChr();
+                if (!chr.startsWith("chr")) {
+                    chr = "chr" + chr;
+                }
+
+                int start = vc.getStart() - 1;
+                int end = vc.getEnd();
+                String id = vc.getID();
+                if(id == null) {
+                    id = ".";
+                }
+                String af = vc.getAttributeAsString("AF", "");
+
+                writer.println(chr + "\t" + start + "\t" + end + "\t" + id + "\t" + af);
+
+            }
+        }
+        finally {
+            if (basicReader != null)
+                basicReader.close();
+            if (writer != null)
+                writer.close();
+        }
+
+
+    }
+
+
+}
