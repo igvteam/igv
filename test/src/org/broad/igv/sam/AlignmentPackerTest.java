214f32e3be616b651cd9d916f08b5f718132124a
James Robinson <jrobinso@broadinstitute.org>
2011-12-27 16:01:48 +0000

    Fix AlignmentPacker unit test
    git-svn-id: https://igv.googlecode.com/svn/trunk@1512 17392f64-ead8-4cea-ae29-09b3ab513800

--- test/src/org/broad/igv/sam/AlignmentPackerTest.java
+++ test/src/org/broad/igv/sam/AlignmentPackerTest.java
@@ -62,14 +62,13 @@ public class AlignmentPackerTest {
         int end = 2250144;
          * */
 
-        String path = "/broad/1KG/DCC_merged/freeze4/NA12878.ceu.daughter.bam";
-        String serverURL = "http://www.broadinstitute.org/webservices/igv";
+        String path = "http://www.broadinstitute.org/igvdata/1KG/pilot2Bams/NA12878.SLX.bam";
         String chr = "1";
         int start = 557000;  //98751004; //
         int end = 558000; //98751046; //
         boolean contained = false;
 
-        ResourceLocator rl = new ResourceLocator(serverURL, path);
+        ResourceLocator rl = new ResourceLocator(path);
 
         BAMRemoteQueryReader bamReader = new BAMRemoteQueryReader(rl);
         CloseableIterator<Alignment> iter = bamReader.query(chr, start, end, contained);
