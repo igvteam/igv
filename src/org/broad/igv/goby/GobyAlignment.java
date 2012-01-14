7a2ea9ca986b1493162e2de546343877bc815da0
Fabien Campagne <fac2003@gmail.com>
2012-01-14 00:09:34 +0000

    Enable display of quality scores in GobyAlignment.
    git-svn-id: https://igv.googlecode.com/svn/trunk@1621 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/goby/GobyAlignment.java
+++ src/org/broad/igv/goby/GobyAlignment.java
@@ -135,7 +135,7 @@ public class GobyAlignment implements Alignment {
                     final int offset = j + var.getPosition() + i - 1 + leftPadding;
                     if (offset>0 && offset < readBases.length) {
                         readBases[offset] = (byte) to.charAt(i);
-                        if (offset < toQuality.size()) {
+                        if (i < toQuality.size()) {
                             readQual[offset] = toQuality.byteAt(i);
                         }
                     }
