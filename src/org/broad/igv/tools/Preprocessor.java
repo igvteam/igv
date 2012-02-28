d0ac44245c425263a3a8c0339ddb8d661c944eb6
James Robinson <jrobinso@broadinstitute.org>
2012-02-28 18:24:13 +0000

    Updated error message in igvotols
    git-svn-id: https://igv.googlecode.com/svn/trunk@1978 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/tools/Preprocessor.java
+++ src/org/broad/igv/tools/Preprocessor.java
@@ -784,9 +784,7 @@ public class Preprocessor implements DataConsumer {
                 AffectiveLogParser parser = new AffectiveLogParser(iFile.getAbsolutePath(), this);
                 parser.parse();
             } else {
-                String extension = getExtension(iFile.getAbsolutePath());
-                out.println("Error: cannot 'tile' files of type " + extension);
-                out.println("Valid file extensions are: .cn, .xcn, .cn, .snp, .wig, and .gct");
+                out.println("Error: cannot files of type '" + tmp + "' to TDF format.");
             }
         }
     }
