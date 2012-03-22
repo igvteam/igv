71c23cbc117787cfd0896f817470282ef99063e3
Jacob Silterra <jacob@broadinstitute.org>
2012-03-22 22:03:15 +0000

    Improve test for setting maxDepth, test setting specific track
    git-svn-id: https://igv.googlecode.com/svn/trunk@2128 17392f64-ead8-4cea-ae29-09b3ab513800

--- test/src/org/broad/igv/batch/CommandExecutorTest.java
+++ test/src/org/broad/igv/batch/CommandExecutorTest.java
@@ -26,11 +26,13 @@ import org.broad.igv.util.ResourceLocator;
 import org.broad.igv.util.TestUtils;
 import org.junit.*;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.*;
 
 /**
  * User: jacob
@@ -90,25 +92,65 @@ public class CommandExecutorTest {
     }
 
     @Test
-    public void setMaxDepth() throws Exception {
+    public void testSetMaxDepth() throws Exception {
         String file = TestUtils.LARGE_DATA_DIR + "/HG00171.hg18.bam";
-        List<Track> loadedTracks = igv.load(new ResourceLocator(file));
+        igv.loadResources(Arrays.asList(new ResourceLocator(file)));
+
+        setCheckMaxDepth(5);
+        setCheckMaxDepth(50);
+    }
+
+    private void setCheckMaxDepth(int maxDepth) {
+        String res = exec.execute("maxDepth " + maxDepth);
+        assertFalse(res.contains("UNKNOWN"));
+
+        List<Track> curTracks = igv.getAllTracks(false);
+        int checked = 0;
+        for (Track track : curTracks) {
+            if (track instanceof AlignmentTrack) {
+                assertEquals(maxDepth, ((AlignmentTrack) track).getMaxDepth());
+                checked++;
+            }
+        }
+        assertTrue("No alignmenttracks found", checked > 0);
+    }
+
+    @Test
+    public void testSetMaxDepthByTrack() throws Exception {
+
+        String name1 = "HG00171.hg18.bam";
+        String name2 = "HG00171.hg18.sam";
+        String file1 = TestUtils.LARGE_DATA_DIR + File.separator + name1;
+        String file2 = TestUtils.LARGE_DATA_DIR + File.separator + name2;
+        List<ResourceLocator> resources = Arrays.asList(new ResourceLocator(file1), new ResourceLocator(file2));
+        igv.loadResources(resources);
 
         setCheckMaxDepth(5);
         setCheckMaxDepth(50);
 
+        int md = 100;
+        exec.execute("maxDepth " + md + " " + name1);
+
+        List<AlignmentTrack> tracks = getAlTracks(igv.getAllTracks(false));
+        AlignmentTrack track1 = tracks.get(0);
+        AlignmentTrack track2 = tracks.get(1);
+
+
+        assertEquals(md, track1.getMaxDepth());
+        assertEquals(50, track2.getMaxDepth());
     }
 
-    private void setCheckMaxDepth(int maxDepth) {
-        exec.execute("setMaxDepth " + maxDepth);
-
-        List<Track> curTracks = igv.getAllTracks(false);
-        for (Track track : curTracks) {
-            if (track instanceof AlignmentTrack) {
-                assertEquals(maxDepth, ((AlignmentTrack) track).getMaxDepth());
+    private List<AlignmentTrack> getAlTracks(List<Track> tracks) {
+        List<AlignmentTrack> out = new ArrayList<AlignmentTrack>(tracks.size());
+        for (Track t : tracks) {
+            try {
+                out.add((AlignmentTrack) t);
+            } catch (ClassCastException e) {
+                continue;
             }
         }
-    }
 
+        return out;
+    }
 
 }
