03cb3d69800181094558f3bc54fda1a069c1a4f3
Neva Cherniavsky <neva@broadinstitute.org>
2012-04-12 17:23:24 +0000

    Adding sum for normalization.  (We don't seem to use gridSize, we could get rid of it?)
    git-svn-id: https://igv.googlecode.com/svn/trunk@2280 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/hic/data/DensityFunction.java
+++ src/org/broad/igv/hic/data/DensityFunction.java
@@ -14,28 +14,37 @@ import java.util.Map;
  */
 public class DensityFunction {
 
-    double[] density;
-    int gridSize;
+    private double[] density;
+    //private int gridSize;
     private int nPoints;
-    Map<Integer, Double> normFactors;
+    private Map<Integer, Double> normFactors;
+    private double sum;
 
     public DensityFunction(int gridSize, double[] densities, Map<Integer, Double> normFactors) {
-        this.gridSize = gridSize;
+        //this.gridSize = gridSize;
         this.density = densities;
         this.nPoints = densities.length;
         this.normFactors = normFactors;
+        this.sum = 0;
+        for (int i=0; i<this.nPoints; i++)
+            sum += this.density[i];
     }
 
     public DensityFunction(DensityCalculation calculation) {
         this(calculation.getGridSize(), calculation.getDensityAvg(), calculation.getNormalizationFactors());
     }
 
+    public double getSum() {
+        return sum;
+    }
+    
     public double getDensity(int chrIdx, int distance) {
 
        // double normFactor = normFactors.containsKey(chrIdx) ? normFactors.get(chrIdx) : 1.0;
-      //  normFactor *= .8;
+       // normFactor *= .8;
+        //   multiplied by this in Python:  (float(matrix.sum())/controlmatrix.sum())
         double normFactor = 1;
-        // Norm factor essentially present for backwards compatibility
+
         if (distance >= nPoints) {
 
             return density[nPoints - 1] / normFactor;
