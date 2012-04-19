5f2d64b36a161767f31c8f6b4877acecfbd12ff6
Jacob Silterra <jacob@broadinstitute.org>
2012-04-19 14:39:46 +0000

    Add comments and apple.laf.useScreenMenuBar=true
    git-svn-id: https://igv.googlecode.com/svn/trunk@2316 17392f64-ead8-4cea-ae29-09b3ab513800

--- scripts/igv.sh
+++ scripts/igv.sh
@@ -1,1 +1,4 @@
-java -Xmx750m -jar `dirname $0`/igv.jar $*
+#apple.laf.useScreenMenuBar for Macs, to put menu bar at top of screen
+#-Xmx750m indicates 750 mb of memory, adjust number up or down as desired
+#Script must be in the same directory as igv.jar
+java -Dapple.laf.useScreenMenuBar=true -Xmx750m -jar `dirname $0`/igv.jar $*
