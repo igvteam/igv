6d36e6b0ff6b01a87de97043dac84bdcdc018600
Jacob Silterra <jacob@broadinstitute.org>
2012-04-19 14:39:43 +0000

    Tweak error handling in GWASParser
    git-svn-id: https://igv.googlecode.com/svn/trunk@2315 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/gwas/GWASParser.java
+++ src/org/broad/igv/gwas/GWASParser.java
@@ -1,26 +1,18 @@
 /*
- * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ * Copyright (c) 2007-2012 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
  *
  * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
  * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
- *
- * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
- * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
- * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
- * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
- * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
- * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
- * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
- * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
  */
 package org.broad.igv.gwas;
 
 import org.apache.log4j.Logger;
 import org.broad.igv.exceptions.ParserException;
 import org.broad.igv.feature.genome.Genome;
-import org.broad.igv.ui.IGV;
 import org.broad.igv.util.ParsingUtils;
 import org.broad.igv.util.ResourceLocator;
 import org.broad.tribble.readers.AsciiLineReader;
@@ -281,19 +273,19 @@ public class GWASParser {
                     }
 
                     // Check if the p-value is NA
-                    if (!tokens[pCol].trim().equals("NA")) {
+                    if (!tokens[pCol].trim().equalsIgnoreCase("NA")) {
                         double p;
 
                         try {
-                            p = Double.parseDouble(tokens[pCol].trim());
-                            if(p <= 0) {
-                                throw new ParserException("Column " + pCol + " must be a positive numeric value.", reader.getCurrentLineNumber(), nextLine);
+                            p = Double.parseDouble(tokens[pCol]);
+                            if (p <= 0) {
+                                throw new NumberFormatException();
                             }
                             // Transform to -log10
                             p = -log10(p);
 
                         } catch (NumberFormatException e) {
-                            throw new ParserException("Column " + pCol + " must be a positive numeric value.", reader.getCurrentLineNumber(), nextLine);
+                            throw new ParserException("Column " + pCol + " must be a positive numeric value. Found " + tokens[pCol], reader.getCurrentLineNumber(), nextLine);
                         }
 
 
@@ -320,7 +312,7 @@ public class GWASParser {
             }
             return gData;
 
-        }  catch (Exception e) {
+        } catch (Exception e) {
             if (nextLine != null && reader.getCurrentLineNumber() != 0) {
                 throw new ParserException(e.getMessage(), e, reader.getCurrentLineNumber(), nextLine);
             } else {
