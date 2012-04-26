da19b466c1d13a2e3519a33b745fc126fadc237a
Jacob Silterra <jacob@broadinstitute.org>
2012-04-26 13:32:48 +0000

    Add more notes to javadoc in PackedFeatures
    git-svn-id: https://igv.googlecode.com/svn/trunk@2390 17392f64-ead8-4cea-ae29-09b3ab513800

--- src/org/broad/igv/track/PackedFeatures.java
+++ src/org/broad/igv/track/PackedFeatures.java
@@ -1,19 +1,12 @@
 /*
- * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ * Copyright (c) 2007-2012 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
  *
  * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
  * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
- *
- * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
- * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
- * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
- * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
- * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
- * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
- * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
- * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
  */
 
 package org.broad.igv.track;
@@ -26,6 +19,10 @@ import org.broad.tribble.Feature;
 import java.util.*;
 
 /**
+ * Represents a table of features, packed so there is no overlap.
+ * Features are packed into rows, accessible via {@link #getRows}
+ * <br/>
+ *
  * dhmay changing 20110213.  I've changed a few things to make it possible to subclass this class. The
  * changes are mostly innocuous, but some are odd, e.g., the iter argument to packFeatures() doesn't declare
  * its type.  This is necessary so that packFeatures() can be overridden.
