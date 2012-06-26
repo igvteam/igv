50f920e3ec15ae6ae7c9e4043bc681a364383b37
Jacob Silterra <jacob@broadinstitute.org>
2012-06-26 13:00:14 -0400

    Add another test to GFFFeatureSourceTest

--- test/src/org/broad/igv/track/GFFFeatureSourceTest.java
+++ test/src/org/broad/igv/track/GFFFeatureSourceTest.java
@@ -14,8 +14,11 @@ package org.broad.igv.track;
 import org.broad.igv.AbstractHeadlessTest;
 import org.broad.igv.feature.BasicFeature;
 import org.broad.igv.feature.GFFParser;
+import org.broad.igv.feature.genome.Genome;
+import org.broad.igv.tools.IgvTools;
 import org.broad.igv.util.*;
 import org.broad.igv.util.TestUtils;
+import org.broad.tribble.CloseableTribbleIterator;
 import org.broad.tribble.Feature;
 import org.junit.Test;
 
@@ -24,6 +27,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * User: jacob
@@ -64,5 +68,46 @@ public class GFFFeatureSourceTest extends AbstractHeadlessTest {
             assertEquals(bf.getIdentifier(), sourceFeat.getIdentifier());
             sF++;
         }
+    }
+
+    @Test
+    public void testQuery_01() throws Exception{
+        String filepath = org.broad.igv.util.TestUtils.DATA_DIR + "gff/aliased.gff";
+        TestUtils.createIndex(filepath);
+        Genome genome = IgvTools.loadGenome(TestUtils.DATA_DIR + "genomes/hg18_truncated_aliased.genome", true);
+
+        GFFFeatureSource source = new GFFFeatureSource(filepath, genome);
+        String chr = "chr5";
+        int start = 120960 - 1;
+        int end = 125258;
+
+        CloseableTribbleIterator<Feature> features = source.getFeatures(chr, start, end);
+        int geneCount = 0;
+        int rnaCount = 0;
+        while(features.hasNext()){
+
+
+            Feature feat = features.next();
+            assertEquals(chr, feat.getChr());
+
+            BasicFeature bf = (BasicFeature) feat;
+
+            String id = bf.getIdentifier().toLowerCase();
+            if(id.contains("gene")) geneCount++;
+            if(id.contains("rna")) rnaCount++;;
+
+
+            if("gene21".equals(id)){
+                assertEquals(0, bf.getExonCount());
+                assertEquals("gene", bf.getType());
+            }
+            if("rna22".equals(id)){
+                assertEquals(6, bf.getExonCount());
+                assertEquals("mRNA", bf.getType());
+            }
+        }
+        assertEquals(2, geneCount);
+        assertEquals(2, rnaCount);
+
     }
 }
