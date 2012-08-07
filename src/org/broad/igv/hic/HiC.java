9837f1325c272dca4adec28c0352b13390d328fd
nchernia <nchernia@gmail.com>
2012-08-07 18:06:58 -0400

    More error handling for eigenvectors

--- src/org/broad/igv/hic/HiC.java
+++ src/org/broad/igv/hic/HiC.java
@@ -297,6 +297,7 @@ public class HiC {
                     str += "Are you sure you want to proceed?";
                     int ans = JOptionPane.showConfirmDialog(mainWindow, str, "Confirm calculation", JOptionPane.YES_NO_OPTION);
                     if (ans == JOptionPane.NO_OPTION) {
+                        mainWindow.setViewEigenvector(false);
                         return null;
                     }
                 }
