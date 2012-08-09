beb5a26cca33d0bd32d5a4074351ef027a3a364e
Jim Robinson <jrobinso@broadinstitute.org>
2012-08-09 14:36:48 -0400

    Fix for loading password protected files in parallel (e.g. from session)

--- src/org/broad/igv/session/IGVSessionReader.java
+++ src/org/broad/igv/session/IGVSessionReader.java
@@ -531,7 +531,7 @@ public class IGVSessionReader implements SessionReader {
                 // Run synchronously if in batch mode or if there are no "track" elments, or if this is an alignment file
                 // EVERYTHING IS RUN SYNCHRONOUSLY FOR NOW UNTIL WE CAN FIGURE OUT WHAT TO DO TO PREVENT MULTIPLE
                 // AUTHENTICATION DIALOGS
-                if (true || isAlignment || Globals.isBatch() || !hasTrackElments) {
+                if (isAlignment || Globals.isBatch() || !hasTrackElments) {
                     synchronousLoads.add(runnable);
                 } else {
                     Thread t = new Thread(runnable);
