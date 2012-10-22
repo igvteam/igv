898467ee5f21b8d3881674bc31f4bac0a96e1f96
Jim Robinson <jrobinso@broadinstitute.org>
2012-10-22 13:49:28 -0400

    Disabled test

--- test/src/org/broad/igv/hic/data/DatasetReaderV2Test.java
+++ test/src/org/broad/igv/hic/data/DatasetReaderV2Test.java
@@ -10,24 +10,24 @@ import org.junit.Test;
  *         Time: 10:36 AM
  */
 public class DatasetReaderV2Test {
-    @Test
+
     public void testRead() throws Exception {
-        String path = "/Users/jrobinso/projects/hic/New/MiSeq_GM_HINDIII.hic";
-
-        DatasetReaderV2 reader = new DatasetReaderV2(path);
-
-        Dataset ds = reader.read();
-
-        Chromosome chr1 = new ChromosomeImpl(14, "14", 0);
-        Chromosome chr2 = chr1;
-
-        Matrix matrix = ds.getMatrix(chr1, chr2);
-
-        MatrixZoomData mzd = matrix.getObservedMatrix(0);
-
-        Block b = mzd.getBlock(0) ;
-
-        b.getContactRecords();
+//        String path = "/Users/jrobinso/projects/hic/New/MiSeq_GM_HINDIII.hic";
+//
+//        DatasetReaderV2 reader = new DatasetReaderV2(path);
+//
+//        Dataset ds = reader.read();
+//
+//        Chromosome chr1 = new ChromosomeImpl(14, "14", 0);
+//        Chromosome chr2 = chr1;
+//
+//        Matrix matrix = ds.getMatrix(chr1, chr2);
+//
+//        MatrixZoomData mzd = matrix.getObservedMatrix(0);
+//
+//        Block b = mzd.getBlock(0) ;
+//
+//        b.getContactRecords();
 
     }
 }
