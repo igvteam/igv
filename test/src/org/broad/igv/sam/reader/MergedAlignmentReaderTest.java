98c6fb780f2218ac7c3647578c0bb8ebb4636ed2
Jacob Silterra <jacob@broadinstitute.org>
2012-11-05 14:15:19 -0500

    Change MergedAlignmentReaderTest to extend AbstractHeadlessTest

--- test/src/org/broad/igv/sam/reader/MergedAlignmentReaderTest.java
+++ test/src/org/broad/igv/sam/reader/MergedAlignmentReaderTest.java
@@ -1,32 +1,22 @@
 /*
- * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.All Rights Reserved.
+ * Copyright (c) 2007-2012 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
  *
  * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
  * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
- *
- * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
- * WARRANTIES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
- * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
- * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
- * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
- * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
- * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
- * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
  */
 
 package org.broad.igv.sam.reader;
 
-import net.sf.samtools.SAMFileHeader;
 import net.sf.samtools.util.CloseableIterator;
-import org.broad.igv.Globals;
+import org.broad.igv.AbstractHeadlessTest;
 import org.broad.igv.sam.Alignment;
 import org.broad.igv.tools.IGVToolsTest;
 import org.broad.igv.util.FileUtils;
 import org.broad.igv.util.TestUtils;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
@@ -41,19 +31,7 @@ import static junit.framework.Assert.assertEquals;
  * User: jacob
  * Date: 2012/01/25
  */
-public class MergedAlignmentReaderTest {
-
-
-    @Before
-    public void setUp() throws Exception {
-        Globals.setHeadless(true);
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
+public class MergedAlignmentReaderTest extends AbstractHeadlessTest {
 
     @Test
     public void testSimpleRead() throws Exception {
