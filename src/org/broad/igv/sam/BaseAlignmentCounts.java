0895dd2a32b45571fd1a6d7ef0ae8b97bff3ff48
Jim Robinson <jrobinso@broadinstitute.org>
2012-12-11 21:15:12 -0500

    Fix bug affecting counts of deletions which follow insertions (i.e. to the right of an insertion).

--- src/org/broad/igv/sam/BaseAlignmentCounts.java
+++ src/org/broad/igv/sam/BaseAlignmentCounts.java
@@ -114,10 +114,13 @@ abstract public class BaseAlignmentCounts implements AlignmentCounts {
                     incBlockCounts(b, isNegativeStrand); //alignment.isNegativeStrand());
 
                     // Count deletions
-                    if (gapTypes != null && lastBlockEnd >= 0 && gapIdx < gapTypes.length &&
-                            gapTypes[gapIdx] == SamAlignment.DELETION) {
-                        for (int pos = lastBlockEnd; pos < b.getStart(); pos++) {
-                            incrementDeletion(pos, isNegativeStrand);
+                    if (gapTypes != null
+                            && (lastBlockEnd >= 0)
+                            && (gapIdx < gapTypes.length)) {
+                        if (gapTypes[gapIdx] == SamAlignment.DELETION) {
+                            for (int pos = lastBlockEnd; pos < b.getStart(); pos++) {
+                                incrementDeletion(pos, isNegativeStrand);
+                            }
                         }
                         gapIdx++;
                     }
@@ -147,28 +150,26 @@ abstract public class BaseAlignmentCounts implements AlignmentCounts {
 
         StringBuffer buf = new StringBuffer();
         int totalCount = getTotalCount(pos);
-        buf.append("Total count: " + totalCount + "<br>");
+        buf.append("Total count: " + totalCount);
         for (char c : nucleotides) {
             int negCount = getNegCount(pos, (byte) c);
             int posCount = getPosCount(pos, (byte) c);
             int count = negCount + posCount;
             int percent = (int) Math.round(((float) count) * 100 / totalCount);
             char cU = Character.toUpperCase(c);
-            buf.append(cU + "      : " + count);
-            if (count == 0) {
-                buf.append("<br>");
-            } else {
-                buf.append("  (" + percent + "%,     " + posCount + "+,   " + negCount + "- )<br>");
+            buf.append("<br>" + cU + "      : " + count);
+            if (count != 0) {
+                buf.append("  (" + percent + "%,     " + posCount + "+,   " + negCount + "- )");
             }
         }
 
         int delCount = getDelCount(pos);
         if (delCount > 0) {
-            buf.append("DEL: " + delCount);
+            buf.append("<br>DEL: " + delCount);
         }
         int insCount = getInsCount(pos);
         if (insCount > 0) {
-            buf.append("INS: " + insCount);
+            buf.append("<br>INS: " + insCount);
         }
 
         return buf.toString();
