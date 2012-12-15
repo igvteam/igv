f0749396b6de33f86c23d146faba1aa55dadb7ac
Jim Robinson <jrobinso@broadinstitute.org>
2012-12-14 20:49:14 -0500

    Fix pre & post ext factor bug (reported by Alex)

--- src/org/broad/igv/tools/CoverageCounter.java
+++ src/org/broad/igv/tools/CoverageCounter.java
@@ -363,9 +363,9 @@ public class CoverageCounter {
                                 // If both postExtFactor and extFactor are specified, postExtFactor takes precedence
                                 if (postExtFactor > 0) {
                                     if (readNegStrand) {
-                                        adjustedStart = Math.max(0, blockStart - postExtFactor);
+                                        adjustedStart = Math.max(0, blockEnd - postExtFactor);
                                     } else {
-                                        adjustedEnd = blockEnd + postExtFactor;
+                                        adjustedEnd = blockStart + postExtFactor;
                                     }
 
                                 } else if (extFactor > 0) {
