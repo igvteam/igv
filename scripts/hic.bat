1217172354bb948f6c211c1d95fbb9c97388f8cf
Jim Robinson <jrobinso@broadinstitute.org>
2012-12-19 00:35:46 -0500

    Change default target for igvtools script to "build both zips"

--- scripts/hic.bat
+++
@@ -1,4 +0,0 @@
-::Get the current batch file's short path
-for %%x in (%0) do set BatchPath=%%~dpsx
-for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
-java -Xmx750m -Dsun.java2d.noddraw=true -jar %BatchPath%\hic.jar  %*
