8f5ed9fbd4d6ffa58ce06b26da26af85b923a2b4
Jim Robinson <jrobinso@broadinstitute.org>
2013-02-25 11:53:49 -0500

    Chromosome name sorting fix

---
+++ test/src/org/broad/igv/feature/genome/ChromosomeComparatorTest.java
@@ -0,0 +1,29 @@
+package org.broad.igv.feature.genome;
+
+import org.broad.igv.feature.Chromosome;
+import org.broad.igv.feature.ChromosomeImpl;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author jrobinso
+ *         Date: 2/13/13
+ *         Time: 2:21 PM
+ */
+public class ChromosomeComparatorTest {
+
+
+    // Test chr names which resolve to numbers too large for "int".  In response to bug report.
+
+    @Test
+    public void testCompareLargeNumbers() throws Exception {
+        long num1 = 7000000037415152l;
+        long num2 = 7000000037415153l;
+        ChromosomeComparator comp = new ChromosomeComparator(0);
+        Chromosome chr1 = new ChromosomeImpl(0, String.valueOf(num1), 1 );
+        Chromosome chr2 = new ChromosomeImpl(0, String.valueOf(num2), 1 );
+        int value = comp.compare(chr1, chr2);
+        assertTrue(value < 0);
+    }
+}
