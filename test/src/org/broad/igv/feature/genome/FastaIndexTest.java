c71e0c6242a072446d2ed4329f8f116508cd0e0d
Jacob Silterra <jacob@broadinstitute.org>
2013-03-01 14:41:34 -0800

    Reorganize fasta index tests, to separate tests for creating index from reading one in

--- test/src/org/broad/igv/feature/genome/FastaIndexTest.java
+++ test/src/org/broad/igv/feature/genome/FastaIndexTest.java
@@ -12,15 +12,18 @@
 package org.broad.igv.feature.genome;
 
 import org.broad.igv.AbstractHeadlessTest;
-import org.broad.igv.exceptions.DataLoadException;
 import org.broad.igv.util.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,11 +34,10 @@ import static junit.framework.Assert.*;
  */
 public class FastaIndexTest extends AbstractHeadlessTest {
 
-    static String indexPath = "http://www.broadinstitute.org/igvdata/test/fasta/ci2_test.fa.fai";
+    static String indexPath = TestUtils.DATA_DIR + "fasta/ci2_test.fa.fai";
     private FastaIndex index;
 
     public static void main(String[] args) throws IOException {
-
         long numContigs = (long) 1.4e6;
         String outfilepath = TestUtils.DATA_DIR + numContigs + "contigs.fasta";
         genManyContigFasta(outfilepath, 80, numContigs);
@@ -102,175 +104,5 @@ public class FastaIndexTest extends AbstractHeadlessTest {
         assertEquals("bytesPerLine", 51, entry.getBytesPerLine());
     }
 
-
-    @Test
-    public void testCreateIndex_01() throws Exception {
-        String inPath = TestUtils.DATA_DIR + "fasta/ecoli_out.padded2.fasta";
-        String outPath = TestUtils.DATA_DIR + "out/ecoli_out.padded2.fasta.fai";
-        File outFile = new File(outPath);
-        outFile.delete();
-        outFile.deleteOnExit();
-
-        FastaUtils.createIndexFile(inPath, outPath);
-
-        FastaIndex index = new FastaIndex(outPath);
-        assertEquals(1, index.getSequenceNames().size());
-        String contig = "NC_000913_bb";
-        assertNotNull(index.getIndexEntry(contig));
-    }
-
-    public void tstCreateIndex_02(String inPath) throws Exception {
-        String outPath = inPath + ".fai";
-        File outFile = new File(outPath);
-        outFile.delete();
-        outFile.deleteOnExit();
-
-        FastaUtils.createIndexFile(inPath, outPath);
-
-        FastaIndex index = new FastaIndex(outPath);
-        assertEquals(2, index.getSequenceNames().size());
-        String tA = "my:testA";
-        String tG = "my:testG";
-        String[] contigs = {tA, tG};
-        for (String contig : contigs) {
-            assertNotNull(index.getIndexEntry(contig));
-        }
-
-        FastaIndex.FastaSequenceIndexEntry entry = index.getIndexEntry(tA);
-        int tAbasesPL = 58;
-
-        //We assume that the test file will have LF line endings
-        //regardless of the platform it's on. May change this in the future.
-        //git default checkouts may change line endings
-        int bytesAtEnd = TestUtils.getBytesAtEnd(inPath);
-
-        int tAbytesPL = tAbasesPL + bytesAtEnd;
-
-        assertEquals(tAbasesPL, entry.getBasesPerLine());
-        assertEquals(tAbytesPL, entry.getBytesPerLine());
-        assertEquals(9 + bytesAtEnd, entry.getPosition());
-        int tAsize = 7 * tAbasesPL + 29;
-        assertEquals(tAsize, entry.getSize());
-        assertEquals(tA, entry.getContig());
-
-        entry = index.getIndexEntry(tG);
-
-        int tGbasesPL = 56;
-        int tGbytesPL = tGbasesPL + bytesAtEnd;
-        assertEquals(tGbasesPL, entry.getBasesPerLine());
-        assertEquals(tGbytesPL, entry.getBytesPerLine());
-        //Starting position is from tA start + tA length + length of header line
-        //Since "size" is number of bases, and "position" is bytes, this
-        //may look weird
-        long tGpos = tAsize + 8*bytesAtEnd + index.getIndexEntry(tA).getPosition() + 9 + bytesAtEnd;
-        assertEquals(tGpos, entry.getPosition());
-        int tGsize = 5 * tGbasesPL + 26;
-        assertEquals(tGsize, entry.getSize());
-        assertEquals(tG, entry.getContig());
-
-        GenomeManager manager = GenomeManager.getInstance();
-        Genome genome = manager.loadGenome(inPath, null);
-        String tAseq = new String(genome.getSequence(tA, 0, tAsize));
-        assertEquals(tAsize, tAseq.length());
-        String remmed = tAseq.replaceAll("A", "");
-        assertEquals(0, remmed.length());
-
-        String tGseq = new String(genome.getSequence(tG, 0, tGsize));
-        assertEquals(tGsize, tGseq.length());
-        remmed = tGseq.replaceAll("G", "");
-        assertEquals(0, remmed.length());
-    }
-
-
-    @Test
-    public void testCreateIndexUncompressed() throws Exception {
-        String inPath = TestUtils.DATA_DIR + "fasta/fasta_2contigs.fa";
-        tstCreateIndex_02(inPath);
-    }
-
-    @Test(expected = DataLoadException.class)
-    public void testCreateIndexUneven() throws Exception {
-        String inPath = TestUtils.DATA_DIR + "fasta/fasta_uneven.fa";
-        String outPath = TestUtils.DATA_DIR + "out/tmp.fai";
-        File outFile = new File(outPath);
-        outFile.delete();
-        outFile.deleteOnExit();
-
-        FastaUtils.createIndexFile(inPath, outPath);
-    }
-
-    @Test(expected = DataLoadException.class)
-    public void testCreateIndexBlankLines() throws Exception {
-        String inPath = TestUtils.DATA_DIR + "fasta/blank_lines.fas";
-        String outPath = TestUtils.DATA_DIR + "out/tmp.fai";
-        File outFile = new File(outPath);
-        outFile.delete();
-        outFile.deleteOnExit();
-
-        FastaUtils.createIndexFile(inPath, outPath);
-    }
-
-    static Map<String, Integer> testFastaBlanks;
-
-    static {
-        testFastaBlanks = new HashMap<String, Integer>(3);
-        testFastaBlanks.put("Chrom1", 632);
-        testFastaBlanks.put("Chrom2", 284);
-        testFastaBlanks.put("Chrom3", 287);
-    }
-
-    @Test
-    public void testCreateIndexTrailingBlank() throws Exception {
-        String inPath = TestUtils.DATA_DIR + "fasta/trailing_line.fas";
-        tstCreateIndexGoodBlanks(inPath, testFastaBlanks);
-    }
-
-    @Test
-    public void testCreateIndexBlankBetweenContigs() throws Exception {
-        String inPath = TestUtils.DATA_DIR + "fasta/blank_lines_betweencontigs.fas";
-        tstCreateIndexGoodBlanks(inPath, testFastaBlanks);
-    }
-
-    public void tstCreateIndexGoodBlanks(String inPath, Map<String, Integer> expectedSizes) throws Exception {
-        String outPath = TestUtils.DATA_DIR + "out/tmp.fai";
-        File outFile = new File(outPath);
-        outFile.delete();
-        outFile.deleteOnExit();
-
-        FastaUtils.createIndexFile(inPath, outPath);
-
-        FastaIndex seq = new FastaIndex(outPath);
-        assertEquals(expectedSizes.size(), seq.getSequenceNames().size());
-        for (String chrom : expectedSizes.keySet()) {
-            assertEquals((int) expectedSizes.get(chrom), seq.getSequenceSize(chrom));
-        }
-
-
-    }
-
-    @Test
-    public void testCreateIndexEcoli() throws Exception {
-        String inPath = TestUtils.LARGE_DATA_DIR + "ecoli.fasta";
-        String outPath = inPath + ".fai";
-        File outFile = new File(outPath);
-
-        outFile.delete();
-        outFile.deleteOnExit();
-
-        GenomeManager manager = GenomeManager.getInstance();
-        Genome genome = manager.loadGenome(inPath, null);
-        String chr = "gi|110640213|ref|NC_008253.1|";
-        assertNotNull(genome.getChromosome(chr));
-        //See http://www.ncbi.nlm.nih.gov/nuccore/110640213
-        assertEquals(4938920, genome.getTotalLength());
-
-        String beg = "ATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAG";
-        int begloc = 30 - 1;
-        int endloc = begloc + beg.length();
-
-        byte[] seq = genome.getSequence(chr, begloc, endloc);
-        String sseq = new String(seq);
-        assertEquals(beg, sseq);
-    }
 
 }
