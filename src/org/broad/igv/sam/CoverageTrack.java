0a420f93110682ff82dad5309c0338166f110b1e
Jim Robinson <jrobinso@broadinstitute.org>
2013-03-09 14:01:58 -0500

    Bug fix -- precomputed coverage not shown in whole-genome view

--- src/org/broad/igv/sam/CoverageTrack.java
+++ src/org/broad/igv/sam/CoverageTrack.java
@@ -183,7 +183,7 @@ public class CoverageTrack extends AbstractTrack {
         float maxRange = PreferenceManager.getInstance().getAsFloat(PreferenceManager.SAM_MAX_VISIBLE_RANGE);
         float minVisibleScale = (maxRange * 1000) / 700;
 
-        if (context.getScale() < minVisibleScale) {
+        if (context.getScale() < minVisibleScale && !context.getChr().equals(Globals.CHR_ALL)) {
             //
             Collection<AlignmentInterval> intervals = null;
             if (dataManager != null) {
