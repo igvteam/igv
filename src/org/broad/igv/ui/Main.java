076d952bae36c915dcad150b2b0328cc864cc059
Jim Robinson <jrobinso@broadinstitute.org>
2013-03-14 11:42:07 -0400

    Bug fix:  Path arguments incorrectly URL decoded, causing GenomeSpace loads to fail for paths with spaces

--- src/org/broad/igv/ui/Main.java
+++ src/org/broad/igv/ui/Main.java
@@ -241,7 +241,7 @@ public class Main {
 
             String[] nonOptionArgs = parser.getRemainingArgs();
             if (nonOptionArgs != null && nonOptionArgs.length > 0) {
-                String firstArg = StringUtils.decodeURL(nonOptionArgs[0]);  // TODO -- why is this url decoded?
+                String firstArg = nonOptionArgs[0];
                 if (firstArg != null && !firstArg.equals("ignore")) {
                     log.info("Loading: " + firstArg);
                     if (firstArg.endsWith(".xml") || firstArg.endsWith(".php") || firstArg.endsWith(".php3")
