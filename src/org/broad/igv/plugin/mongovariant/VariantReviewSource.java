27f18c46640771df6a68bd36b1ff12fc42655ed5
Jacob Silterra <jsilter@gmail.com>
2013-04-06 18:03:41 -0400

    Only show consensus sites
    rather than all calls

--- src/org/broad/igv/plugin/mongovariant/VariantReviewSource.java
+++ src/org/broad/igv/plugin/mongovariant/VariantReviewSource.java
@@ -75,8 +75,7 @@ public class VariantReviewSource implements FeatureSource<VCFVariant> {
         SiteSelector criteria = new SiteSelector(parser);
         //Convert from 0-based to 1-based
         criteria.addInterval(chromoNameToStandard(chr), start + 1, end);
-        //TODO DON'T COMMIT THIS
-        SiteIterator<MongoVariantContext> iterator  = kb.getCalls(criteria);//kb.getConsensusSites(criteria);
+        SiteIterator<MongoVariantContext> iterator  = kb.getConsensusSites(criteria);
         List<VCFVariant> variants = new ArrayList<VCFVariant>();
         while(iterator.hasNext()){
             MongoVariantContext mvc = iterator.next();
