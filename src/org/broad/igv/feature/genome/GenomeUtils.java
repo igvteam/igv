7a537568fd19c3ad7adb9e3f0d126bb08adee3d9
Jacob Silterra <jacob@broadinstitute.org>
2013-04-23 14:55:58 -0400

    Remove unused and obsolete org.broad.igv.util.GenomeUtils

--- src/org/broad/igv/feature/genome/GenomeUtils.java
+++ src/org/broad/igv/feature/genome/GenomeUtils.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2007-2013 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ */
+
 package org.broad.igv.feature.genome;
 
 import org.broad.igv.feature.Chromosome;
@@ -32,6 +43,13 @@ public class GenomeUtils {
     }
 
 
+    /**
+     * Create .chrom.sizes file for each genome found in the {@code genomeListPath}, and write it out to
+     * {@code directory}
+     * @param directory
+     * @param genomeListPath
+     * @throws IOException
+     */
     public static void exportAllChromSizes(File directory, String genomeListPath) throws IOException {
 
         //<Server-Side Genome List>
@@ -102,7 +120,10 @@ public class GenomeUtils {
     }
 
     /**
-     * Export a "chrom.sizes" file
+     * Export a "chrom.sizes" file for the specified genome
+     * @param directory output directory
+     * @param genome
+     * @throws FileNotFoundException
      */
     public static void exportChromSizes(File directory, Genome genome) throws FileNotFoundException {
 
