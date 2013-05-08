6ed8340902ea1cfbc1757f88c6e965448877c8d4
Jim Robinson <jrobinso@broadinstitute.org>
2013-05-08 09:52:03 -0400

    TribbleFeatureSource - test for file closing

---
+++ test/src/org/broad/igv/track/TribbleFeatureSourceTest.java
@@ -0,0 +1,15 @@
+package org.broad.igv.track;
+
+import org.junit.Test;
+
+/**
+ * @author jrobinso
+ *         Date: 5/8/13
+ *         Time: 8:32 AM
+ */
+public class TribbleFeatureSourceTest {
+    @Test
+    public void testGetFeatures() throws Exception {
+
+    }
+}
