5e93666898ddb156f99c8556a29d72249672e08f
Jim Robinson <jrobinso@broadinstitute.org>
2013-05-08 13:05:41 -0400

    Disabled problematic test

--- test/src/org/broad/igv/track/TribbleFeatureSourceTest.java
+++ test/src/org/broad/igv/track/TribbleFeatureSourceTest.java
@@ -3,6 +3,7 @@ package org.broad.igv.track;
 import org.broad.igv.Globals;
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.broad.igv.util.TestUtils;
 
@@ -23,25 +24,28 @@ public class TribbleFeatureSourceTest {
      */
     @Test
     public void testCloseFileHandles() throws Exception {
-        String file =  org.broad.igv.util.TestUtils.DATA_DIR +  "bed/Unigene.sample.sorted.bed";
+        String file = org.broad.igv.util.TestUtils.DATA_DIR + "bed/Unigene.sample.sorted.bed";
         TribbleFeatureSource featuresSource = new TribbleFeatureSource(file, null, false);
-        for(int i=0; i<10000; i++) {
+        for (int i = 0; i < 10000; i++) {
             featuresSource.getFeatures("chr2", 178709699, 178711955);
         }
     }
 
-   @Test
-    public void testFileHandleNumberNonincreasing() throws Exception{
+
+
+    @Ignore   // Ignored, getting Cannot run program "lsof" error on test server. The test is redundant to testCloseFileHandles()
+    @Test
+    public void testFileHandleNumberNonincreasing() throws Exception {
         int trials = 100;
         int maxDiff = 2;
         Assume.assumeTrue(Globals.IS_LINUX || Globals.IS_MAC);
         int baseFileHandles = TestUtils.getNumberOpenFileHandles();
 
-       String file =  org.broad.igv.util.TestUtils.DATA_DIR +  "bed/Unigene.sample.sorted.bed";
-       TribbleFeatureSource featuresSource = new TribbleFeatureSource(file, null, false);
+        String file = org.broad.igv.util.TestUtils.DATA_DIR + "bed/Unigene.sample.sorted.bed";
+        TribbleFeatureSource featuresSource = new TribbleFeatureSource(file, null, false);
 
-       for(int tri=0; tri < trials; tri++){
-           featuresSource.getFeatures("chr2", 178709699, 178711955);
+        for (int tri = 0; tri < trials; tri++) {
+            featuresSource.getFeatures("chr2", 178709699, 178711955);
             int curFileHandles = TestUtils.getNumberOpenFileHandles();
             String msg = "Number of open file handles deviates too much from base:\n";
             msg += "base: " + baseFileHandles + " current: " + curFileHandles + " trial number: " + tri;
