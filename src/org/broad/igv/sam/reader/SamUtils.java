f2109cf99e67f479ff7a18219eaf307f93ea56e7
Jacob Silterra <jacob@broadinstitute.org>
2013-05-29 15:49:09 -0400

    Use log instead of System.out

--- src/org/broad/igv/sam/reader/SamUtils.java
+++ src/org/broad/igv/sam/reader/SamUtils.java
@@ -1,19 +1,12 @@
 /*
- * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ * Copyright (c) 2007-2013 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
  *
  * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
  * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
- *
- * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
- * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
- * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
- * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
- * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
- * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
- * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
- * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
  */
 package org.broad.igv.sam.reader;
 
@@ -26,6 +19,7 @@ import net.sf.samtools.CigarElement;
 import net.sf.samtools.CigarOperator;
 import net.sf.samtools.SAMRecord;
 import net.sf.samtools.util.StringUtil;
+import org.apache.log4j.Logger;
 import org.broad.igv.DirectoryManager;
 import org.broad.igv.Globals;
 import org.broad.igv.ui.IGV;
@@ -38,6 +32,8 @@ import java.io.IOException;
  * @author jrobinso
  */
 public class SamUtils {
+
+    private static Logger log = Logger.getLogger(SamUtils.class);
 
     private static final byte ZERO_BYTE = "0".getBytes()[0];
     private static final byte NINE_BYTE = "9".getBytes()[0];
@@ -79,7 +75,7 @@ public class SamUtils {
             AlignmentIndexer indexer = AlignmentIndexer.getInstance(samFile, null, null);
             FeatureIndex index = null;
             try {
-                System.out.println("Creating index");
+                log.info("Creating index " + newIdxFile.getAbsolutePath());
                 index = indexer.createSamIndex(newIdxFile);
             } catch (IOException e) {
                 e.fillInStackTrace();
