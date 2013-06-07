1d8a3fe4929583fd4d704eb6dfb6ecf3d730d756
Jacob Silterra <jacob@broadinstitute.org>
2013-06-07 16:47:04 -0400

    Update IGVCommandBarTest now that we no longer throw spurious events

--- test/src/org/broad/igv/ui/IGVCommandBarTest.java
+++ test/src/org/broad/igv/ui/IGVCommandBarTest.java
@@ -12,17 +12,15 @@
 package org.broad.igv.ui;
 
 import com.google.common.eventbus.Subscribe;
+import org.apache.log4j.Logger;
 import org.broad.igv.feature.AminoAcidManager;
 import org.broad.igv.feature.genome.GenomeManager;
 import org.broad.igv.ui.action.SearchCommand;
 import org.broad.igv.ui.event.ViewChange;
 import org.broad.igv.ui.panel.FrameManager;
 import org.fest.swing.fixture.FrameFixture;
-import org.fest.swing.fixture.JComboBoxFixture;
 import org.fest.swing.fixture.JTextComponentFixture;
 import org.junit.*;
-
-import javax.swing.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -35,12 +33,16 @@ import static org.junit.Assert.assertTrue;
  */
 public class IGVCommandBarTest extends AbstractHeadedTest {
 
+    private static Logger log = Logger.getLogger(IGVCommandBarTest.class);
+
     private static FrameFixture frame;
 
     //We use asynchronous events, and there are asserts in there
     //Here we check that the events were actually dispatched and handled
     private int expectedEvents = 0;
     private int actualEvents = expectedEvents;
+
+    private boolean registered;
 
     /**
      * Because loading IGV takes so long, we only load it once per class.
@@ -52,12 +54,6 @@ public class IGVCommandBarTest extends AbstractHeadedTest {
     public static void setUpClass() throws Exception {
         AbstractHeadedTest.setUpClass();
         frame = new FrameFixture(IGV.getMainFrame());
-
-
-        //FrameFixture frame = new FrameFixture(IGV.getMainFrame());
-        //JPanelFixture contentFixture = frame.panel("contentPane");
-        //JPanelFixture commandBar = frame.panel("igvCommandBar");
-        //JComboBoxFixture chromoBox = frame.comboBox("chromosomeComboBox");
     }
 
     @Before
@@ -65,14 +61,16 @@ public class IGVCommandBarTest extends AbstractHeadedTest {
         super.setUp();
         this.actualEvents = 0;
         this.expectedEvents = 0;
+        this.registered = false;
     }
-
-
 
     @After
     public void tearDown() throws Exception{
         System.out.println("Expected Events: " + expectedEvents + " Actual Events: " + actualEvents);
         assertTrue("Event handler not triggered properly", actualEvents >= expectedEvents);
+        if(registered){
+            FrameManager.getDefaultFrame().getEventBus().unregister(this);
+        }
         super.tearDown();
     }
 
@@ -92,43 +90,42 @@ public class IGVCommandBarTest extends AbstractHeadedTest {
     }
 
     //Tacky state variable, but eh, it's just a test
-    private String enterText = null;
+    private volatile String enterText = null;
 
     private void tstChromoNav(String chromoText) throws Exception {
-        registerEventHandler(2);
+        registerEventHandler(1);
 
         JTextComponentFixture searchFixture = frame.textBox("searchTextField");
         searchFixture.deleteText();
-        enterText = chromoText;
+        this.enterText = chromoText;
 
         //Make sure search box has focus
         searchFixture.focus();
         searchFixture.requireFocused();
         searchFixture.requireEmpty();
 
-        searchFixture.enterText(enterText);
+        searchFixture.enterText(chromoText);
         frame.button("goButton").click();
     }
 
     private void registerEventHandler(int expectedEvents) {
         this.expectedEvents += expectedEvents;
-        FrameManager.getDefaultFrame().getEventBus().register(this);
+        if(!registered){
+            FrameManager.getDefaultFrame().getEventBus().register(this);
+            this.registered = true;
+        }
     }
 
-    /**
-     * This is a little funny, we are actually responding to a spurious ChromosomeChangeCause
-     * sent when the dropdown changes
-     * @param e
-     */
     @Subscribe
     public void receiveChromoChange(ViewChange.ChromosomeChangeCause e){
-        if(e.source instanceof JComboBox){
+        if(e.source instanceof SearchCommand){
             actualEvents++;
-            JComboBoxFixture comboBox = frame.comboBox("chromosomeComboBox");
-            comboBox.requireSelection(enterText);
-        }else if(e.source instanceof SearchCommand){
-            actualEvents++;
-            assertEquals(enterText, e.chrName);
+            try {
+                assertEquals(this.enterText, e.chrName);
+            } catch (Exception e1) {
+                log.error(e1.getMessage(), e1);
+                actualEvents = Integer.MIN_VALUE;
+            }
         }else{
             actualEvents = Integer.MIN_VALUE;
             throw new AssertionError("Got a ChromosomeChangeCause event from unexpected source: " + e.source);
@@ -138,15 +135,13 @@ public class IGVCommandBarTest extends AbstractHeadedTest {
     @Test
     public void testChromoNav_CodonTable() throws Exception {
         //Make sure that translation table DOES NOT change when we change chromosomes
-        Assume.assumeTrue("hg18".equals(GenomeManager.getInstance().getGenomeId()));
+        Assume.assumeTrue("hg18.unittest".equals(GenomeManager.getInstance().getGenomeId()));
 
         tstChromoNav("chr1");
         assertEquals(1, AminoAcidManager.getInstance().getCodonTable().getId());
 
         tstChromoNav("chrM");
         assertEquals(1, AminoAcidManager.getInstance().getCodonTable().getId());
-
-
     }
 
 }
