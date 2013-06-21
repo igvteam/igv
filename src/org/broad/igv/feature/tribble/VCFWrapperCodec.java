97977073b7ec31326e7e156f3f0e7e3472151aa7
Jacob Silterra <jacob@broadinstitute.org>
2013-06-21 09:59:00 -0400

    Have VCFWrapperCodec try replacing ",.," with ",0," on NumberFormatException
    This is a tad hacky, but it shouldn't affect lines without this defect so the possible damage is limited.
    The correction is also not limited to the genotype field

--- src/org/broad/igv/feature/tribble/VCFWrapperCodec.java
+++ src/org/broad/igv/feature/tribble/VCFWrapperCodec.java
@@ -11,7 +11,9 @@
 
 package org.broad.igv.feature.tribble;
 
+import org.apache.log4j.Logger;
 import org.broad.igv.feature.genome.Genome;
+import org.broad.igv.variant.Variant;
 import org.broad.igv.variant.vcf.VCFVariant;
 import org.broad.tribble.AsciiFeatureCodec;
 import org.broad.tribble.Feature;
@@ -23,6 +25,8 @@ import org.broadinstitute.variant.variantcontext.VariantContext;
  * @date Aug 1, 2011
  */
 public class VCFWrapperCodec extends AsciiFeatureCodec<VCFVariant> {
+
+    private static Logger log = Logger.getLogger(Variant.class);
 
     AsciiFeatureCodec wrappedCodec;
     Genome genome;
@@ -40,7 +44,25 @@ public class VCFWrapperCodec extends AsciiFeatureCodec<VCFVariant> {
 
     @Override
     public VCFVariant decode(String line) {
-        VariantContext vc = (VariantContext) wrappedCodec.decode(line);
+        // VCFCodec supports completely missing fields (which would simply have a ".")
+        // but does not currently support missing only certain elements of a field.
+        // IGV is much more permissive.
+
+        VariantContext vc = null;
+        try {
+            vc = (VariantContext) wrappedCodec.decode(line);
+            //The genotype fields are loaded lazily, we force parsing here to
+            //catch the exception if necessary
+            if (vc != null) vc.getSampleNames();
+        } catch (NumberFormatException e) {
+            String msg = String.format("NumberFormatException on line: %s \n Attempting to reformat by replacing ,., with ,0,", line);
+            log.warn(msg);
+            String refLine = line.replaceAll(",\\.", ",0");
+            refLine = refLine.replaceAll("\\.,", "0,");
+            vc = (VariantContext) wrappedCodec.decode(refLine);
+        }
+
+
         if (vc == null) {
             return null;
         }
