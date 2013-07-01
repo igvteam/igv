82a421c2e3f4ea55fa5fdc734a2e9ecc58eca5e1
Jacob Silterra <jacob@broadinstitute.org>
2013-07-01 17:50:56 -0400

    Update VCFWriterTest to properly use vcf classes

--- test/src/org/broad/igv/variant/vcf/VCFWriterTest.java
+++ test/src/org/broad/igv/variant/vcf/VCFWriterTest.java
@@ -11,55 +11,112 @@
 
 package org.broad.igv.variant.vcf;
 
+import net.sf.samtools.SAMSequenceDictionary;
 import org.broad.igv.AbstractHeadlessTest;
-import org.junit.Ignore;
+import org.broad.igv.feature.tribble.CodecFactory;
+import org.broad.igv.util.TestUtils;
+import org.broad.tribble.AbstractFeatureReader;
+import org.broad.tribble.Feature;
+import org.broad.tribble.FeatureCodec;
+import org.broadinstitute.variant.variantcontext.writer.Options;
+import org.broadinstitute.variant.variantcontext.writer.VariantContextWriter;
+import org.broadinstitute.variant.variantcontext.writer.VariantContextWriterFactory;
+import org.broadinstitute.variant.vcf.VCFHeader;
+import org.broadinstitute.variant.vcf.VCFInfoHeaderLine;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
 
 /**
- * User: jacob
- * Date: 2012/05/11
+ * @author jacob
+ * @date 2012/05/11
  */
-// TODO We don't have the necessary dependencies
-// TODO Writer classes were made package private at some point
-@Ignore
 public class VCFWriterTest extends AbstractHeadlessTest {
 
-//    @Test
-//    public void testWriteHeader() throws Exception {
-//        String inpath = TestUtils.DATA_DIR + "vcf/SRP32_v4.0.vcf";
-//        File outFile = new File(TestUtils.DATA_DIR, "testwriter.vcf");
-//        VCFWriter writer = new StandardVCFWriter(outFile, null);
-//
-//        FeatureCodec codec = CodecFactory.getCodec(inpath, genome);
-//        AbstractFeatureReader<Feature> bfs = AbstractFeatureReader.getFeatureReader(inpath, codec, false);
-//        Iterable<Feature> iter = bfs.iterator();
-//        Object header = bfs.getHeader();
-//
-//        writer.writeHeader((VCFHeader) header);
-//        writer.close();
-//    }
-//
-//    @Test
-//    public void testWriteRecords() throws Exception {
-//        String inpath = TestUtils.DATA_DIR + "vcf/SRP32_v4.0.vcf";
-//        File outFile = new File(TestUtils.DATA_DIR, "testwriter.vcf");
-//        VCFWriter writer = new StandardVCFWriter(outFile, null);
-//
-//        FeatureCodec codec = CodecFactory.getCodec(inpath, genome);
-//        AbstractFeatureReader<Feature> bfs = AbstractFeatureReader.getFeatureReader(inpath, codec, false);
-//        Iterable<Feature> iter = bfs.iterator();
-//        Object header = bfs.getHeader();
-//
-//        writer.writeHeader((VCFHeader) header);
-//
-//        for (Feature feat : iter) {
-//            if (feat instanceof VCFVariant) {
-//                VCFVariant var = (VCFVariant) feat;
-//                writer.add(var.getVariantContext());
-//            }
-//        }
-//
-//        writer.close();
-//
-//
-//    }
+    String inpath = TestUtils.DATA_DIR + "vcf/SRP32_v4.sorted.0.vcf";
+    File outFile = new File(TestUtils.TMP_OUTPUT_DIR, "testwriterout.vcf");
+
+    private VariantContextWriter getWriter() {
+        SAMSequenceDictionary seqDict = new SAMSequenceDictionary();
+        EnumSet<Options> options = VariantContextWriterFactory.DEFAULT_OPTIONS;
+        options.add(Options.ALLOW_MISSING_FIELDS_IN_HEADER);
+        VariantContextWriter writer = VariantContextWriterFactory.create(outFile, seqDict, options);
+        return writer;
+    }
+
+    @Test
+    public void testWriteHeader() throws Exception {
+
+        FeatureCodec codec = CodecFactory.getCodec(inpath, genome);
+        AbstractFeatureReader<Feature> bfs = AbstractFeatureReader.getFeatureReader(inpath, codec, false);
+        VCFHeader header0 = (VCFHeader) bfs.getHeader();
+
+        VariantContextWriter writer = getWriter();
+        writer.writeHeader(header0);
+
+        AbstractFeatureReader<Feature> bfs1 = AbstractFeatureReader.getFeatureReader(outFile.getAbsolutePath(), codec, false);
+        VCFHeader header1 = (VCFHeader) bfs1.getHeader();
+
+        assertHeadersEquals(header0, header1);
+    }
+
+    private void assertHeadersEquals(VCFHeader header0, VCFHeader header1) {
+
+
+        assertEquals(header0.getColumnCount(), header1.getColumnCount());
+        assertEquals(header0.getGenotypeSamples(), header1.getGenotypeSamples());
+        assertEquals(header0.getContigLines(), header1.getContigLines());
+
+        for (VCFInfoHeaderLine line0 : header0.getInfoHeaderLines()) {
+            VCFInfoHeaderLine line1 = header1.getInfoHeaderLine(line0.getID());
+            assertEquals(line0.getCount(), line1.getCount());
+            assertEquals(line0.getType(), line1.getType());
+            assertEquals(line0.getDescription(), line1.getDescription());
+            assertEquals(0, line0.compareTo(line1));
+        }
+    }
+
+    @Test
+    public void testWriteRecords() throws Exception {
+
+        FeatureCodec codec = CodecFactory.getCodec(inpath, genome);
+        AbstractFeatureReader<VCFVariant> bfs = AbstractFeatureReader.getFeatureReader(inpath, codec, false);
+        Iterable<VCFVariant> iter0 = bfs.iterator();
+        List<VCFVariant> list0 = new ArrayList<VCFVariant>();
+        VCFHeader header0 = (VCFHeader) bfs.getHeader();
+
+        VariantContextWriter writer = getWriter();
+        writer.writeHeader(header0);
+
+        for (VCFVariant var : iter0) {
+            writer.add(var.getVariantContext());
+            list0.add(var);
+        }
+
+        writer.close();
+
+        AbstractFeatureReader<VCFVariant> bfs1 = AbstractFeatureReader.getFeatureReader(outFile.getAbsolutePath(), codec, false);
+        Iterable<VCFVariant> iter1 = bfs1.iterator();
+        VCFHeader header1 = (VCFHeader) bfs1.getHeader();
+
+        assertHeadersEquals(header0, header1);
+        int n = 0;
+
+        for (VCFVariant var1 : iter1) {
+            VCFVariant var0 = list0.get(n++);
+            TestUtils.assertFeaturesEqual(var0, var1);
+
+            assertEquals(var0.getType(), var1.getType());
+            assertEquals(var0.getID(), var1.getID());
+            assertEquals(var0.getSampleNames(), var1.getSampleNames());
+            assertEquals(var0.getAttributes(), var1.getAttributes());
+        }
+
+    }
 }
