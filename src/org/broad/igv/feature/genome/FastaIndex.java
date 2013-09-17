1316b028a04530285952c4432f1ed8001875cc16
Jacob Silterra <jacob@broadinstitute.org>
2013-09-17 14:24:32 -0400

    Improve doc for FastaIndex

--- src/org/broad/igv/feature/genome/FastaIndex.java
+++ src/org/broad/igv/feature/genome/FastaIndex.java
@@ -1,19 +1,12 @@
 /*
- * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ * Copyright (c) 2007-2013 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
  *
  * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
  * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
- *
- * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
- * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
- * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
- * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
- * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
- * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
- * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
- * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
  */
 
 package org.broad.igv.feature.genome;
@@ -22,7 +15,8 @@ import org.apache.log4j.Logger;
 import org.broad.igv.Globals;
 import org.broad.igv.util.ParsingUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
@@ -30,8 +24,8 @@ import java.util.Set;
  * Representation of a fasta (.fai) index.  This is a modified version of a similar class in Picard, but extended
  * to handle loading by URLs.
  *
- * author: jrobinso
- * Date: 8/7/11
+ * @auther jrobinso
+ * @since 2011 Aug 7
  */
 public class FastaIndex {
 
@@ -65,6 +59,7 @@ public class FastaIndex {
      * <p/>
      * Example index file
      * <p/>
+     * sequenceName size locationInFile basesPerLine bytesPerLine
      * chr01p	6220112	8	50	51
      * chr02q	8059593	6344531	50	51
      * chr03q	5803340	14565324	50	51
@@ -146,46 +141,35 @@ public class FastaIndex {
         }
 
         /**
-         * Gets the contig associated with this entry.
-         *
-         * @return String representation of the contig.
+         * @return The contig.
          */
         public String getContig() {
             return contig;
         }
 
         /**
-         * Gets the position of this contig within the fasta.
-         *
-         * @return seek position within the fasta.
+         * @return seek position within the fasta, in bytes
          */
         public long getPosition() {
             return position;
         }
 
         /**
-         * Gets the size, in bytes, of the data in the contig.
-         *
-         * @return size of the contig bases in bytes.
+         * @return size of the contig bases, in bytes.
          */
         public long getSize() {
             return size;
         }
 
         /**
-         * Gets the number of bases in a given line.
-         *
-         * @return Number of bases in the fasta line.
+         * @return Number of bases in a given fasta line
          */
         public int getBasesPerLine() {
             return basesPerLine;
         }
 
         /**
-         * How many bytes (bases + whitespace) are consumed by the
-         * given line?
-         *
-         * @return Number of bytes in a line.
+         * @return Number of bytes (bases + whitespace) in a line.
          */
         public int getBytesPerLine() {
             return bytesPerLine;
@@ -193,8 +177,6 @@ public class FastaIndex {
 
 
         /**
-         * For debugging.  Emit the contents of each contig line.
-         *
          * @return A string representation of the contig line.
          */
         public String toString() {
