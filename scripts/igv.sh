dd2ef57d4b79b0bd6e74157e0dc699ac488c9df8
Jacob Silterra <jacob@broadinstitute.org>
2013-10-23 22:42:36 -0400

    Add command line flag to launch script so IGV is displayed in menu bar on macs

--- scripts/igv.sh
+++ scripts/igv.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #apple.laf.useScreenMenuBar for Macs, to put menu bar at top of screen
+#-Xdock:name again for Macs, sets the name in menu bar
 #-Xmx2000m indicates 2000 mb of memory, adjust number up or down as needed
 #-Dproduction=true disables non-released and development features
 #Script must be in the same directory as igv.jar
 prefix=`dirname $(readlink $0 || echo $0)`
 exec java -Xmx2000m \
+    -Xdock:name="IGV" \
 	-Dproduction=true \
 	-Dapple.laf.useScreenMenuBar=true \
 	-Djava.net.preferIPv4Stack=true \
