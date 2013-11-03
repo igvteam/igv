f635fe908bc1c96d515aa2ea5e34ce8c53780cde
Jim Robinson <jrobinso@broadinstitute.org>
2013-11-02 20:54:41 -0400

    Add missing class BrowseEncodeAction

---
+++ src/org/broad/igv/ui/action/BrowseEncodeAction.java
@@ -0,0 +1,53 @@
+package org.broad.igv.ui.action;
+
+import org.apache.log4j.Logger;
+import org.broad.igv.ui.IGV;
+import org.broad.igv.util.ResourceLocator;
+import org.broad.igv.util.encode.EncodeFileBrowser;
+import org.broad.igv.util.encode.EncodeFileRecord;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jrobinso
+ *         Date: 11/2/13
+ *         Time: 6:39 PM
+ */
+public class BrowseEncodeAction extends MenuAction {
+
+    private static Logger log = Logger.getLogger(BrowseEncodeAction.class);
+
+    IGV igv;
+
+    public BrowseEncodeAction(String label, int mnemonic, IGV igv) {
+        super(label, null, mnemonic);
+        this.igv = igv;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+        try {
+            EncodeFileBrowser browser = EncodeFileBrowser.getInstance();
+            browser.setVisible(true);
+            if(browser.isCanceled()) return;
+
+           java.util.List<EncodeFileRecord> records = browser.getSelectedRecords();
+            if(records.size() > 0) {
+                List<ResourceLocator> locators = new ArrayList<ResourceLocator>(records.size());
+                for(EncodeFileRecord record : records) {
+                    locators.add(new ResourceLocator(record.getPath()));
+                }
+                igv.loadTracks(locators);
+            }
+
+
+        } catch (IOException e) {
+            log.error("Error opening Encode browser", e);
+        }
+    }
+}
