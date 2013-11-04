d00762784293a5188b6d1a9dd99c685dc77a241e
Jacob Silterra <jacob@broadinstitute.org>
2013-11-04 14:04:02 -0500

    Refactor SaveSessionMenuAction slightly
    Extract actual session saving portion into its own function

--- src/org/broad/igv/ui/action/SaveSessionMenuAction.java
+++ src/org/broad/igv/ui/action/SaveSessionMenuAction.java
@@ -1,19 +1,12 @@
 /*
- * Copyright (c) 2007-2011 by The Broad Institute of MIT and Harvard.  All Rights Reserved.
+ * Copyright (c) 2007-2013 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
  *
  * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
  * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
- *
- * THE SOFTWARE IS PROVIDED "AS IS." THE BROAD AND MIT MAKE NO REPRESENTATIONS OR
- * WARRANTES OF ANY KIND CONCERNING THE SOFTWARE, EXPRESS OR IMPLIED, INCLUDING,
- * WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, NONINFRINGEMENT, OR THE ABSENCE OF LATENT OR OTHER DEFECTS, WHETHER
- * OR NOT DISCOVERABLE.  IN NO EVENT SHALL THE BROAD OR MIT, OR THEIR RESPECTIVE
- * TRUSTEES, DIRECTORS, OFFICERS, EMPLOYEES, AND AFFILIATES BE LIABLE FOR ANY DAMAGES
- * OF ANY KIND, INCLUDING, WITHOUT LIMITATION, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
- * ECONOMIC DAMAGES OR INJURY TO PROPERTY AND LOST PROFITS, REGARDLESS OF WHETHER
- * THE BROAD OR MIT SHALL BE ADVISED, SHALL HAVE OTHER REASON TO KNOW, OR IN FACT
- * SHALL KNOW OF THE POSSIBILITY OF THE FOREGOING.
  */
 /*
  * To change this template, choose Tools | Templates
@@ -24,7 +17,6 @@ package org.broad.igv.ui.action;
 //~--- non-JDK imports --------------------------------------------------------
 
 import org.apache.log4j.Logger;
-import org.broad.igv.Globals;
 import org.broad.igv.PreferenceManager;
 import org.broad.igv.session.Session;
 import org.broad.igv.session.SessionWriter;
@@ -36,7 +28,6 @@ import org.broad.igv.ui.util.FileDialogUtils;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -45,21 +36,18 @@ import java.io.IOException;
 public class SaveSessionMenuAction extends MenuAction {
 
     static Logger log = Logger.getLogger(SaveSessionMenuAction.class);
-
-    // TODO -- The batch referenceFrame is likely to be used by many actions. Move this
-    // member to a base class ?
-    IGV mainFrame;
+    IGV igv;
 
     /**
-     * Constructs ...
+     *
      *
      * @param label
      * @param mnemonic
-     * @param mainFrame
+     * @param igv
      */
-    public SaveSessionMenuAction(String label, int mnemonic, IGV mainFrame) {
+    public SaveSessionMenuAction(String label, int mnemonic, IGV igv) {
         super(label, null, mnemonic);
-        this.mainFrame = mainFrame;
+        this.igv = igv;
     }
 
     /**
@@ -73,7 +61,7 @@ public class SaveSessionMenuAction extends MenuAction {
 
         File sessionFile = null;
 
-        String currentSessionFilePath = mainFrame.getSession().getPath();
+        String currentSessionFilePath = igv.getSession().getPath();
 
         String initFile = currentSessionFilePath == null ? UIConstants.DEFAULT_SESSION_FILE : currentSessionFilePath;
         sessionFile = FileDialogUtils.chooseFile("Save Session",
@@ -83,7 +71,7 @@ public class SaveSessionMenuAction extends MenuAction {
 
 
         if (sessionFile == null) {
-            mainFrame.resetStatusMessage();
+            igv.resetStatusMessage();
             return;
         }
 
@@ -93,30 +81,39 @@ public class SaveSessionMenuAction extends MenuAction {
             sessionFile = new File(filePath + ".xml");
         }
 
-        mainFrame.setStatusBarMessage("Saving session to " + sessionFile.getAbsolutePath());
+        igv.setStatusBarMessage("Saving session to " + sessionFile.getAbsolutePath());
 
 
         final File sf = sessionFile;
         WaitCursorManager.CursorToken token = WaitCursorManager.showWaitCursor();
         try {
-
-            Session currentSession = mainFrame.getSession();
-            currentSession.setPath(sf.getAbsolutePath());
-            (new SessionWriter()).saveSession(currentSession, sf);
-
+            saveSession(igv, sf);
             // No errors so save last location
             PreferenceManager.getInstance().setLastSessionDirectory(sf.getParentFile());
 
         } catch (Exception e2) {
-            JOptionPane.showMessageDialog(mainFrame.getMainFrame(), "There was an error writing to " + sf.getName() + "(" + e2.getMessage() + ")");
+            JOptionPane.showMessageDialog(igv.getMainFrame(), "There was an error writing to " + sf.getName() + "(" + e2.getMessage() + ")");
             log.error("Failed to save session!", e2);
         } finally {
             WaitCursorManager.removeWaitCursor(token);
-            mainFrame.resetStatusMessage();
+            igv.resetStatusMessage();
 
 
         }
 
 
+    }
+
+    /**
+     * Saves current IGV session to {@code targetFile}. As a side effect,
+     * sets the current sessions path (does NOT set the last session directory)
+     * @param igv
+     * @param targetFile
+     * @throws IOException
+     */
+    public static void saveSession(IGV igv, File targetFile) throws IOException{
+        Session currentSession = igv.getSession();
+        currentSession.setPath(targetFile.getAbsolutePath());
+        (new SessionWriter()).saveSession(currentSession, targetFile);
     }
 }
