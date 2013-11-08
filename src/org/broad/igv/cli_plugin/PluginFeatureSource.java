a2cc1472d1c7d409215fa5a693e6fc9aa1a460a7
Jacob Silterra <jacob@broadinstitute.org>
2013-11-08 16:17:58 -0500

    Workaround for the fact that BEDTools can't read blank files
    If we try to export a blank feature file, add in 1 feature which is unlikely to overlap with anything. This should have the same effect as no features at all

--- src/org/broad/igv/cli_plugin/PluginFeatureSource.java
+++ src/org/broad/igv/cli_plugin/PluginFeatureSource.java
@@ -12,6 +12,7 @@
 package org.broad.igv.cli_plugin;
 
 import org.apache.log4j.Logger;
+import org.broad.igv.feature.Locus;
 import org.broad.igv.feature.LocusScore;
 import org.broad.igv.sam.Alignment;
 import org.broad.igv.sam.AlignmentTrack;
@@ -22,6 +23,7 @@ import org.broad.igv.track.Track;
 import org.broad.tribble.Feature;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -52,6 +54,10 @@ public class PluginFeatureSource<E extends Feature, D extends Feature> extends P
 
         FeatureTrack fTrack = (FeatureTrack) track;
         List<Feature> features = fTrack.getFeatures(chr, start, end);
+        //Workaround for BEDTools bug, github #88, it can't read an empty file
+        if(features.size() == 0){
+            features = Arrays.<Feature>asList(new Locus("XXXchr0XXX", 0, 1));
+        }
         return super.createTempFile(features, argument);
     }
 
