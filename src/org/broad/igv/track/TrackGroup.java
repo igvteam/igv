46e5c8aa668a26b361ef05dc9a315efee3a9bba8
Jacob Silterra <jacob@broadinstitute.org>
2013-11-18 10:16:05 -0500

    Remove redundant comparator classes
    Github #70

--- src/org/broad/igv/track/TrackGroup.java
+++ src/org/broad/igv/track/TrackGroup.java
@@ -188,7 +188,7 @@ public class TrackGroup {
     public void sortByAttributes(final String[] attributeNames,
                                  final boolean[] ascending) {
         if ((tracks != null) && !tracks.isEmpty()) {
-            Comparator<Track> comparator = new TrackAttributeComparator(attributeNames, ascending);
+            Comparator<Track> comparator = new AttributeComparator.TrackAttributeComparator(attributeNames, ascending);
 
             // Step 1, remove non-sortable tracks and remember position
             List<Track> nonsortableTracks = new ArrayList<Track>();
@@ -209,7 +209,7 @@ public class TrackGroup {
             // variant tracks are implemented as monoliths, with sample rows internal to the track.
             for (Track t : tracks) {
                 if (t instanceof org.broad.igv.variant.VariantTrack) {
-                    ((org.broad.igv.variant.VariantTrack) t).sortSamples(new SampleAttributeComparator(attributeNames, ascending));
+                    ((org.broad.igv.variant.VariantTrack) t).sortSamples(new AttributeComparator.SampleAttributeComparator(attributeNames, ascending));
                 }
             }
 
@@ -389,100 +389,4 @@ public class TrackGroup {
 
     }
 
-    private abstract static class AttributeComparator<T> implements Comparator<T> {
-
-        private final String[] attributeNames;
-        private final boolean[] ascending;
-
-        AttributeComparator(String[] attributeNames, boolean[] ascending) {
-            assert attributeNames.length == ascending.length;
-            this.attributeNames = attributeNames;
-            this.ascending = ascending;
-        }
-
-        protected abstract String getAttributeValue(T track, String attName);
-
-        public int compare(T t1, T t2) {
-            // Loop through the attributes in order (primary, secondary, tertiary, ...).  The
-            // first attribute to yield a non-zero comparison wins
-            for (int i = 0; i < attributeNames.length; i++) {
-                String attName = attributeNames[i];
-
-                if (attName != null) {
-                    String value1 = getAttributeValue(t1, attName);
-                    String value2 = getAttributeValue(t2, attName);
-
-                    boolean isNumeric = AttributeManager.getInstance().isNumeric(attName);
-
-                    int c = 0;
-                    if (isNumeric) {
-                        double d1;
-                        try {
-                            d1 = Double.parseDouble(value1);
-                        } catch (NumberFormatException e) {
-                            d1 = Double.MIN_VALUE;
-                        }
-                        double d2;
-                        try {
-                            d2 = Double.parseDouble(value2);
-                        } catch (NumberFormatException e) {
-                            d2 = Double.MIN_VALUE;
-                        }
-                        c = Double.compare(d1, d2);
-                    } else {
-                        c = value1.compareTo(value2);
-                    }
-
-                    if (c != 0) {
-                        return ascending[i] ? c : -c;
-                    }
-
-                }
-            }
-
-            // All compares are equal
-            return 0;
-        }
-    }
-
-    private static class TrackAttributeComparator extends AttributeComparator<Track> {
-
-        public TrackAttributeComparator(String[] attributeNames, boolean[] ascending) {
-            super(attributeNames, ascending);
-        }
-
-        protected String getAttributeValue(Track track, String attName) {
-            String value = track.getAttributeValue(attName);
-
-            if (value == null) {
-                value = "";
-            }
-
-            return value.toLowerCase();
-        }
-    }
-
-
-    /**
-     * Sort samples by attribute value; logic copied wholesale from
-     * AttributeComparator, probably some refactoring could be done
-     * to minimize the duplicated code
-     */
-    private static class SampleAttributeComparator extends AttributeComparator<String> {
-
-        public SampleAttributeComparator(String[] attributeNames, boolean[] ascending) {
-            super(attributeNames, ascending);
-        }
-
-        protected String getAttributeValue(String sample, String attName) {
-            String value = AttributeManager.getInstance().getAttribute(sample, attName);
-
-            if (value == null) {
-                value = "";
-            }
-
-            return value.toLowerCase();
-        }
-
-    }
 }
