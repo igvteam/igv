04768fc6c905d69c9a83a19f9c4f903f479b46a4
Jacob Silterra <jacob@broadinstitute.org>
2013-11-22 09:17:38 -0500

    Change reused url string to use static field

--- test/src/org/broad/igv/util/ftp/FTPUtilsTest.java
+++ test/src/org/broad/igv/util/ftp/FTPUtilsTest.java
@@ -1,10 +1,22 @@
+/*
+ * Copyright (c) 2007-2013 The Broad Institute, Inc.
+ * SOFTWARE COPYRIGHT NOTICE
+ * This software and its documentation are the copyright of the Broad Institute, Inc. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever. The Broad Institute is not responsible for its use, misuse, or functionality.
+ *
+ * This software is licensed under the terms of the GNU Lesser General Public License (LGPL),
+ * Version 2.1 which is available at http://www.opensource.org/licenses/lgpl-2.1.php.
+ */
+
 package org.broad.igv.util.ftp;
 
-import static junit.framework.Assert.*;
-
+import org.broad.igv.util.TestUtils;
 import org.junit.Test;
 
 import java.net.URL;
+
+import static junit.framework.Assert.*;
 
 
 /**
@@ -15,7 +27,7 @@ public class FTPUtilsTest {
 
     @Test
     public void testResourceAvailable() throws Exception {
-        URL goodUrl = new URL("ftp://ftp.broadinstitute.org/pub/igv/TEST/test.txt");
+        URL goodUrl = new URL(TestUtils.AVAILABLE_FTP_URL);
         assertTrue(FTPUtils.resourceAvailable(goodUrl));
 
         URL nonExistentURL = new URL("ftp://ftp.broadinstitute.org/pub/igv/TEST/doesntExist");
