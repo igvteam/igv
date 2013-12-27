3d57b7263d40ad2eb5217e01e5c1d76c17075b8d
Jacob Silterra <jacob@broadinstitute.org>
2013-12-27 15:32:03 -0500

    Add scratch code for exporting alignments to SAM/BAM file
    Not a direct file copy, query alignments and then write them. Should be easily workable for SAM and BAM files

--- test/src/org/broad/igv/sam/BAMFileReaderTest.java
+++ test/src/org/broad/igv/sam/BAMFileReaderTest.java
@@ -16,6 +16,7 @@
 
 package org.broad.igv.sam;
 
+import net.sf.samtools.*;
 import net.sf.samtools.util.CloseableIterator;
 import org.broad.igv.Globals;
 import org.broad.igv.sam.reader.BAMFileReader;
@@ -23,6 +24,7 @@ import org.broad.igv.sam.reader.SAMReader;
 import org.broad.igv.util.TestUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -108,6 +110,71 @@ public class BAMFileReaderTest {
         }
         assertTrue("No data retrieved", count > 0);
         System.out.println("Retrieved " + count + " rows");
+
+    }
+
+    @Ignore("Just scratch code")
+    @Test
+    public void testCopyBAMFileSnippet() {
+
+        String sequence = "chr1";
+        int end = 300000000;
+        int start = end / 5;
+
+        String bamFile = TestUtils.LARGE_DATA_DIR + "HG00171.hg18.bam";
+        SAMFileReader reader = new SAMFileReader(new File(bamFile));
+        reader.setValidationStringency(SAMFileReader.ValidationStringency.SILENT);
+
+        // Hit the index to determine the chunk boundaries for the required data.
+        final SAMFileHeader fileHeader = reader.getFileHeader();
+        final int referenceIndex = fileHeader.getSequenceIndex(sequence);
+
+        String outPath = "tmpbam.bam";
+        File outFile = new File(outPath);
+        File indexFile = new File(outPath.replace(".bam", ".bai"));
+        outFile.delete();
+        indexFile.delete();
+        outFile.deleteOnExit();
+        indexFile.deleteOnExit();
+        SAMFileWriterFactory factory = new SAMFileWriterFactory();
+        //May wish to tinker with options
+        //factory.setMaxRecordsInRam(1000).setUseAsyncIo(true);
+        boolean createIndex = true;
+        factory.setCreateIndex(createIndex);
+        SAMFileWriter writer = factory.makeSAMOrBAMWriter(fileHeader, true, outFile);
+
+        int count = 0;
+        //NOTE: 1-BASED START/END
+        SAMRecordIterator iter = reader.queryOverlapping(sequence, start, end);
+        while (iter.hasNext()) {
+            writer.addAlignment(iter.next());
+            count++;
+        }
+        iter.close();
+        writer.close();
+
+
+        assertEquals("Index file existence unexpected: " + indexFile.getAbsolutePath(), createIndex, indexFile.exists());
+
+        SAMFileReader writtenReader = new SAMFileReader(new File(outPath));
+        writtenReader.setValidationStringency(SAMFileReader.ValidationStringency.SILENT);
+        iter = null;
+        if(createIndex){
+            iter = writtenReader.queryOverlapping(sequence, start, end);
+        }else{
+            iter = writtenReader.iterator();
+        }
+
+        int readCount = 0;
+        while (iter.hasNext()) {
+            readCount++;
+            iter.next();
+        }
+
+        System.out.println(readCount + " alignments read");
+        assertTrue("No alignments read", readCount > 0);
+        assertEquals("Read a different number of alignments than written", count, readCount);
+
 
     }
 
