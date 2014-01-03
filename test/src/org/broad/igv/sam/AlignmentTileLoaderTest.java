0e443137d314ddfa363ec02b438ae524de070313
Jacob Silterra <jacob@broadinstitute.org>
2014-01-03 13:15:44 -0500

    Additional downsampling unit tests Test that we don't downsample when limit set high, and also do another keep-pair test with a different file
    IGV-2065

--- test/src/org/broad/igv/sam/AlignmentTileLoaderTest.java
+++ test/src/org/broad/igv/sam/AlignmentTileLoaderTest.java
@@ -20,6 +20,7 @@ import org.broad.igv.PreferenceManager;
 import org.broad.igv.sam.reader.AlignmentReader;
 import org.broad.igv.sam.reader.AlignmentReaderFactory;
 import org.broad.igv.util.ResourceLocator;
+import org.broad.igv.util.TestUtils;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -41,22 +42,73 @@ public class AlignmentTileLoaderTest extends AbstractHeadlessTest {
      */
     @Test
     //@Ignore("Not implemented yet")
-    public void testKeepPairs() throws Exception {
+    public void testKeepPairsDownsample_01() throws Exception {
         String path = "http://1000genomes.s3.amazonaws.com/data/NA12878/high_coverage_alignment/NA12878.mapped.ILLUMINA.bwa.CEU.high_coverage_pcr_free.20130520.bam";
 
         String sequence = "1";
         int start = 10000-1;
         int end = 11000;
-        int maxDepth = 2;
-        String max_vis = PreferenceManager.getInstance().get(PreferenceManager.SAM_MAX_VISIBLE_RANGE);
+        int maxDepth = -1;
+
+        AlignmentTileLoader.AlignmentTile tile= tstKeepPairsDownsample(path, sequence, start, end, maxDepth);
+        assertTrue(tile.getDownsampledIntervals().size() > 0);
+    }
+
+    /**
+     * Test that sampling keeps pairs together.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testKeepPairsDownsample_02() throws Exception {
+        String path = TestUtils.LARGE_DATA_DIR + "HG00171.hg18.bam";
+
+        String sequence = "chr1";
+        int start = 151766945;
+        int end = 151791197;
+        int maxDepth = 5;
+
+        AlignmentTileLoader.AlignmentTile tile= tstKeepPairsDownsample(path, sequence, start, end, maxDepth);
+        assertTrue(tile.getDownsampledIntervals().size() > 0);
+    }
+
+    /**
+     * Test that sampling keeps pairs together.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testNoDownsample() throws Exception {
+        String path = TestUtils.LARGE_DATA_DIR + "HG00171.hg18.bam";
+
+        String sequence = "chr1";
+        int start = 151766945;
+        int end = 151791197;
+        int maxDepth = 500;
+
+        AlignmentTileLoader.AlignmentTile tile= tstKeepPairsDownsample(path, sequence, start, end, maxDepth);
+        assertEquals(0, tile.getDownsampledIntervals().size());
+
+
+    }
+
+    private AlignmentTileLoader.AlignmentTile tstKeepPairsDownsample(String path, String sequence, int start, int end, int maxDepth) throws Exception{
+
+
+        String oldMaxVis = PreferenceManager.getInstance().get(PreferenceManager.SAM_MAX_VISIBLE_RANGE);
         PreferenceManager.getInstance().put(PreferenceManager.SAM_MAX_VISIBLE_RANGE, "" + (end - start));
+
+        int actMaxDepth = 100;
+        if(maxDepth > 0){
+            actMaxDepth = maxDepth;
+        }
 
         try {
             ResourceLocator loc = new ResourceLocator(path);
             AlignmentReader reader = AlignmentReaderFactory.getReader(loc);
             AlignmentTileLoader loader = new AlignmentTileLoader(reader);
 
-            AlignmentDataManager.DownsampleOptions downsampleOptions = new AlignmentDataManager.DownsampleOptions(true, 50, 100);
+            AlignmentDataManager.DownsampleOptions downsampleOptions = new AlignmentDataManager.DownsampleOptions(true, 50, actMaxDepth);
 
             AlignmentTileLoader.AlignmentTile tile = loader.loadTile(sequence, start, end, null, downsampleOptions, null, null, null);
             List<Alignment> alignments = tile.getAlignments();
@@ -95,9 +147,11 @@ public class AlignmentTileLoaderTest extends AbstractHeadlessTest {
             System.out.println("Number of paired reads: " + pairedReads.size());
             assertTrue("No pairs in test data set", pairedReads.size() > 0);
             assertEquals("Missing " + countmissing + " out of " + pairedReads.size() + " pairs", 0, countmissing);
+            return tile;
         } catch (Exception e) {
-            PreferenceManager.getInstance().put(PreferenceManager.SAM_MAX_VISIBLE_RANGE, max_vis);
             throw e;
+        }finally{
+            PreferenceManager.getInstance().put(PreferenceManager.SAM_MAX_VISIBLE_RANGE, oldMaxVis);
         }
 
     }
