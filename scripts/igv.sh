c770576aea5bafabc91c7bf87ed6034fbb0891c8
jrobinso <jrobinso@broadinstitute.org>
2015-10-29 15:58:44 -0700

    Increase startup shell script memory settings to 4GB

--- scripts/igv.sh
+++ scripts/igv.sh
@@ -2,12 +2,11 @@
 
 #This script is intended for launch on *nix machines
 
-#-Xmx2000m indicates 2000 mb of memory, adjust number up or down as needed
-#-Dproduction=true disables non-released and development features
+#-Xmx4000m indicates 4000 mb of memory, adjust number up or down as needed
 #Script must be in the same directory as igv.jar
 #Add the flag -Ddevelopment = true to use features still in development
 prefix=`dirname $(readlink $0 || echo $0)`
-exec java -Xmx2000m \
+exec java -Xmx4000m \
 	-Dapple.laf.useScreenMenuBar=true \
 	-Djava.net.preferIPv4Stack=true \
 	-jar "$prefix"/igv.jar "$@"
