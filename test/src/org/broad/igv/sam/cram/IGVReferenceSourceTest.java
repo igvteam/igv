16ac140dbc78879f8f2939a5fc6142ff83a0a0c9
Jim Robinson <jrobinso@broadinstitute.org>
2016-05-25 14:42:43 -0700

    CRAM - implement ReferenceSource

---
+++ test/src/org/broad/igv/sam/cram/IGVReferenceSourceTest.java
@@ -0,0 +1,22 @@
+package org.broad.igv.sam.cram;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jrobinso on 5/25/16.
+ */
+public class IGVReferenceSourceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetReferenceBases() throws Exception {
+
+    }
+}
