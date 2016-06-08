9753204eed098f3539f433b5e7f502dc2c5b2d4b
Jim Robinson <jrobinso@broadinstitute.org>
2016-06-07 20:25:42 -0700

    GFF parsing updates -- keep attributes (col 9) for all constituitive mRNA parts.  Fixes #251. Catch all execptions when adding plugins, not just IOException.

--- src/org/broad/igv/feature/GFFFeature.java
+++ src/org/broad/igv/feature/GFFFeature.java
@@ -26,20 +26,54 @@
 package org.broad.igv.feature;
 
 import org.broad.igv.track.WindowFunction;
+import org.broad.igv.util.collections.MultiMap;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author jrobinso
  *         Date: 7/18/13
  *         Time: 9:50 PM
  */
-public class GFFFeature extends AbstractFeature {
-    @Override
-    public String getURL() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+public class GFFFeature extends BasicFeature {
+
+    List<String> componentAttributes = new ArrayList<>();
+
+    public GFFFeature(BasicFeature feature) {
+        super(feature);
+    }
+
+    public GFFFeature(String chr, int start, int end, Strand strand) {
+        super(chr, start, end, strand);
     }
 
     @Override
-    public String getValueString(double position, WindowFunction windowFunction) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public void addExon(Exon region) {
+        super.addExon(region);
+    }
+
+    @Override
+    public void addUTRorCDS(BasicFeature bf) {
+        super.addUTRorCDS(bf);
+    }
+
+    @Override
+    public String getValueString(double position, WindowFunction ignored) {
+
+        StringBuffer valueString = new StringBuffer(super.getValueString(position, ignored));
+        if (componentAttributes.size() > 0) {
+            for (String s : componentAttributes) {
+                valueString.append("---------------------------");
+                valueString.append(s);
+            }
+        }
+        return valueString.toString();
+    }
+
+    public void mergeAttributes(BasicFeature mrnaPart) {
+        StringBuffer buf = new StringBuffer();
+        mrnaPart.getAttributes().printHtml(buf, 100);
+        componentAttributes.add(buf.toString());
     }
 }
--- src/org/broad/igv/track/FeatureTrack.java
+++ src/org/broad/igv/track/FeatureTrack.java
@@ -411,7 +411,7 @@ public class FeatureTrack extends AbstractTrack {
             for (Feature feature : allFeatures) {
                 if (feature != null && feature instanceof IGVFeature) {
                     if (!firstFeature) {
-                        buf.append("<br/>--------------<br/>");
+                        buf.append("<hr><br>");
                     }
 
                     IGVFeature igvFeature = (IGVFeature) feature;
