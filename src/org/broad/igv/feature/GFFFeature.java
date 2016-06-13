069091a0ab6defb3f37db1acb45acb7961b9c143
jrobinso <jrobinso@broadinstitute.org>
2016-06-13 13:08:05 -0400

    More gff popup enhacements

--- src/org/broad/igv/feature/GFFFeature.java
+++ src/org/broad/igv/feature/GFFFeature.java
@@ -63,6 +63,7 @@ public class GFFFeature extends BasicFeature {
 
         StringBuffer valueString = new StringBuffer();
 
+        valueString.append("<b>Type:</b>&nbsp;" + this.type);
         if (attributes != null) {
             valueString.append(getAttributeString());
         }
@@ -78,6 +79,7 @@ public class GFFFeature extends BasicFeature {
 
     public void mergeAttributes(BasicFeature mrnaPart) {
         StringBuffer buf = new StringBuffer();
+        buf.append("<br><b>Type:</b>&nbsp;" + mrnaPart.getType());
         mrnaPart.getAttributes().printHtml(buf, 100);
         componentAttributes.add(buf.toString());
     }
