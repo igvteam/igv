27fafa243795fa82067ce230c26d9f4b64649d29
Jim Robinson <jrobinso@broadinstitute.org>
2016-07-08 13:48:39 -0700

    remove print statement

--- src/org/broad/igv/ga4gh/Ga4ghAPIHelper.java
+++ src/org/broad/igv/ga4gh/Ga4ghAPIHelper.java
@@ -224,7 +224,6 @@ public class Ga4ghAPIHelper {
             pageToken = obj.getAsJsonPrimitive("nextPageToken");
             if (pageToken == null || pageToken.getAsString().equals("")) break;
 
-            System.out.println("" + (++counter) + "   " + alignments.size());
         }
 
         //System.out.println("# pages= " + (10000 - maxPages));
