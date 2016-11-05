5aadd06ebda1c90dd6b8fc7495418bea8fbbbfe9
Jim Robinson <jrobinso@broadinstitute.org>
2016-11-05 10:17:35 -0700

    Remove Mac OS dependency

--- src/org/broad/igv/ui/IGV.java
+++ src/org/broad/igv/ui/IGV.java
@@ -2773,24 +2773,24 @@ public class IGV implements IGVEventObserver {
         return completed;
     }
 
-
-
-    private void getRealDPI() {
-        // find the display device of interest
-        final GraphicsDevice defaultScreenDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-
-        // on OS X, it would be CGraphicsDevice
-        if (defaultScreenDevice instanceof CGraphicsDevice) {
-            final CGraphicsDevice device = (CGraphicsDevice) defaultScreenDevice;
-
-            // this is the missing correction factor, it's equal to 2 on HiDPI a.k.a. Retina displays
-            final int scaleFactor = device.getScaleFactor();
-
-            // now we can compute the real DPI of the screen
-            final double realDPI = scaleFactor * (device.getXResolution() + device.getYResolution()) / 2;
-
-            System.out.println("scale factor = " + scaleFactor + "    realDPI = " + realDPI);
-        }
-    }
+//
+//    NOTE:  MAC ONLY,  WILL NOT COMPILE ON OTHER PLATFORMS
+//    private void getRealDPI() {
+//        // find the display device of interest
+//        final GraphicsDevice defaultScreenDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+//
+//        // on OS X, it would be CGraphicsDevice
+//        if (defaultScreenDevice instanceof CGraphicsDevice) {
+//            final CGraphicsDevice device = (CGraphicsDevice) defaultScreenDevice;
+//
+//            // this is the missing correction factor, it's equal to 2 on HiDPI a.k.a. Retina displays
+//            final int scaleFactor = device.getScaleFactor();
+//
+//            // now we can compute the real DPI of the screen
+//            final double realDPI = scaleFactor * (device.getXResolution() + device.getYResolution()) / 2;
+//
+//            System.out.println("scale factor = " + scaleFactor + "    realDPI = " + realDPI);
+//        }
+//    }
 
 }
