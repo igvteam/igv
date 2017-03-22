06be780c172a082f003b653ce49af74344b91580
Jim Robinson <jrobinso@broadinstitute.org>
2017-03-22 15:16:05 -0700

    Workaround for color pallete initialization problem.  This should not be neccessary.

--- src/org/broad/igv/ui/color/ColorUtilities.java
+++ src/org/broad/igv/ui/color/ColorUtilities.java
@@ -31,10 +31,7 @@ import org.apache.log4j.Logger;
 import org.broad.igv.util.ObjectCache;
 
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 
@@ -295,11 +292,26 @@ public class ColorUtilities {
         return getCompositeColor(Color.white, source, alpha);
     }
 
+    static String colorPalleteString  = "#Set 1\n" +
+            "228, 26, 28; 55, 126, 184; 77, 175, 74; 166, 86, 40; 152, 78, 163; 255, 127, 0; 247, 129, 191; 153, 153, 153; 255, 255, 51;\n" +
+            "#Dark 2\n" +
+            "27, 158, 119; 217, 95, 2; 117, 112, 179; 231, 41, 138; 102, 166, 30; 230, 171, 2; 166, 118, 29; 102, 102, 102;\n" +
+            "#Set 2\n" +
+            "102, 194, 165; 252, 141, 98; 141, 160, 203; 231, 138, 195; 166, 216, 84; 255, 217, 47; 229, 196, 148; 179, 179, 179;\n" +
+            "#Set 3\n" +
+            "141, 211, 199; 255, 255, 179; 190, 186, 218; 251, 128, 114; 128, 177, 211; 253, 180, 98; 179, 222, 105; 252, 205, 229; 217, 217, 217; 188, 128, 189; 204, 235, 197; 255, 237, 111;\n" +
+            "#Pastel 1\n" +
+            "251, 180, 174; 179, 205, 227; 204, 235, 197; 222, 203, 228; 254, 217, 166; 255, 255, 204; 229, 216, 189; 253, 218, 236;\n" +
+            "#Pastel 2\n" +
+            "173, 226, 207; 253, 205, 172; 203, 213, 232; 244, 202, 228; 230, 245, 201; 255, 242, 174; 243, 225, 206;\n" +
+            "#Accent\n" +
+            "127, 201, 127; 190, 174, 212; 253, 192, 134; 255, 255, 153; 56, 108, 176; 240, 2, 127; 191, 91, 23;";
 
     public static Map<String, ColorPalette> loadPalettes() throws IOException {
 
-        InputStream is = ColorUtilities.class.getResourceAsStream("resources/colorPalettes.txt");
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        //InputStream is = ColorUtilities.class.getResourceAsStream("resources/colorPalettes.txt");
+        //BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        BufferedReader br = new BufferedReader(new StringReader(colorPalleteString));
         String nextLine;
 
         palettes = new LinkedHashMap<String, ColorPalette>();
