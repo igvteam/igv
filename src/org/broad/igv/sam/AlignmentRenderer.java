9bb804d67f3a60feb9cc66d0de8589343843e221
Jim Robinson <jrobinso@broadinstitute.org>
2017-06-21 13:00:17 -0700

    Prioritize orienation over insert size for alignment color

--- src/org/broad/igv/sam/AlignmentRenderer.java
+++ src/org/broad/igv/sam/AlignmentRenderer.java
@@ -1071,7 +1071,7 @@ public class AlignmentRenderer implements FeatureRenderer {
             case UNEXPECTED_PAIR:
             case PAIR_ORIENTATION:
                 c = getOrientationColor(alignment, getPEStats(alignment, renderOptions));
-                if (colorOption == ColorOption.PAIR_ORIENTATION) {
+                if (c != DEFAULT_ALIGNMENT_COLOR || colorOption == ColorOption.PAIR_ORIENTATION) {
                     break;
                 }
             case INSERT_SIZE:
