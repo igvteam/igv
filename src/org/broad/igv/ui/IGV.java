3eec5a01837ed39051277ad74357c6cd631b808e
Jim Robinson <jrobinso@broadinstitute.org>
2017-07-05 14:53:21 -0700

    Provide default extension for image files  (.png).  Fixes #438

--- src/org/broad/igv/ui/IGV.java
+++ src/org/broad/igv/ui/IGV.java
@@ -805,6 +805,11 @@ public class IGV implements IGVEventObserver {
 
         String extension = FileUtils.getFileExtension(file.getAbsolutePath());
 
+        if(extension == null) {
+            extension = ".png";
+            file = new File(file.getAbsolutePath() + extension);
+        }
+
         SnapshotFileChooser.SnapshotFileType type = SnapshotFileChooser.getSnapshotFileType(extension);
 
         String message;
