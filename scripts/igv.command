a30986820bf6f1e73df57e08934101c04473275b
Jim Robinson <jrobinso@broadinstitute.org>
2017-10-12 09:50:58 -0700

    Update command lines for Java 9  (JAXB module)

--- scripts/igv.command
+++ scripts/igv.command
@@ -10,6 +10,7 @@
 #Add the flag -Ddevelopment = true to use features still in development
 prefix=`dirname $(readlink $0 || echo $0)`
 exec java -Xmx4000m \
+     --illegal-access=permit --add-modules=java.xml.bind \
     -Xdock:name="IGV" \
 	-Dapple.laf.useScreenMenuBar=true \
 	-Djava.net.preferIPv4Stack=true \
--- scripts/igv.sh
+++ scripts/igv.sh
@@ -7,6 +7,7 @@
 #Add the flag -Ddevelopment = true to use features still in development
 prefix=`dirname $(readlink $0 || echo $0)`
 exec java -Xmx4000m \
+    --illegal-access=permit --add-modules=java.xml.bind \
 	-Dapple.laf.useScreenMenuBar=true \
 	-Djava.net.preferIPv4Stack=true \
 	-jar "$prefix"/igv.jar "$@"
