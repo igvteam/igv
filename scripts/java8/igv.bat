d0e05f3280e1d696a5a572a24fc2ab79d32d67b4
eby <eby@broadinstitute.org>
2018-06-18 15:57:23 +0900

    Removing Java 9 flags from Java 8 launchers
    Not needed as we need separate launchers anyway.

---
+++ scripts/java8/igv.bat
@@ -0,0 +1,4 @@
+::Get the current batch file's short path
+for %%x in (%0) do set BatchPath=%%~dpsx
+for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
+java -Xmx4000m -Dproduction=true -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true -jar %BatchPath%\igv.jar  %*
