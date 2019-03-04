119246fd53c19c880185f04d2da3511915a3d59f
Roman Valls Guimera <brainstorm@nopcode.org>
2019-03-05 00:51:33 +1100

    Bump up Gradle version, remove Java 10 limits (Java 11 bytecode is now supported), make sure Goby counts hit the right files, bump up slf4j api versions

--- src/main/java11/module-info.java
+++ src/main/java11/module-info.java
@@ -29,7 +29,6 @@ module org.igv {
     requires org.apache.batik.svggen;
     requires org.apache.batik.dom;
     requires dsiutils;
-    requires slf4j.api;
 
     // AWS
     requires software.amazon.awssdk.core;
