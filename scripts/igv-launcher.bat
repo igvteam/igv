e44c3aeb8f2adc8aea22cac7893ba0685b1205da
kelepiradam <37572619+kelepiradam@users.noreply.github.com>
2020-05-03 09:18:48 +0300

    Update igv-launcher.bat

--- scripts/igv-launcher.bat
+++ scripts/igv-launcher.bat
@@ -1,3 +1,3 @@
 setlocal
 
-start jdk-11\bin\javaw -showversion --module-path=lib -Xmx8g -Dproduction=true @igv.args -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true --module=org.igv/org.broad.igv.ui.Main 
+start jdk-11\bin\javaw -showversion --module-path=lib -Xmx4g -Dproduction=true @igv.args -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true --module=org.igv/org.broad.igv.ui.Main 
