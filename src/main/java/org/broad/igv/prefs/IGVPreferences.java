bf89b6ac55865e3a9c206380d37cf58259ab8f91
Jim Robinson <jrobinso@broadinstitute.org>
2020-06-19 15:59:44 -0700

    Use native file dialog for image save

--- src/main/java/org/broad/igv/prefs/IGVPreferences.java
+++ src/main/java/org/broad/igv/prefs/IGVPreferences.java
@@ -414,9 +414,9 @@ public class IGVPreferences {
     /**
      * @param directory
      */
-    public void setLastSnapshotDirectory(File directory) {
+    public void setLastSnapshotDirectory(String directory) {
 
-        put(LAST_SNAPSHOT_DIRECTORY, directory.getAbsolutePath());
+        put(LAST_SNAPSHOT_DIRECTORY, directory);
     }
 
     /**
