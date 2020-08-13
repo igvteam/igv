8b11121b6983694205ca898c4d0aa218a292d495
Jim Robinson <jrobinso@broadinstitute.org>
2020-08-13 10:48:27 -0700

    Support "hidden" track for search in json genome definition.  See issue #589

--- src/main/java/org/broad/igv/feature/genome/load/JsonLoader.java
+++ src/main/java/org/broad/igv/feature/genome/load/JsonLoader.java
@@ -4,9 +4,18 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import htsjdk.tribble.CloseableTribbleIterator;
+import htsjdk.tribble.Feature;
+import htsjdk.tribble.FeatureReader;
+import org.apache.log4j.Logger;
+import org.broad.igv.feature.FeatureDB;
+import org.broad.igv.feature.LocusScore;
+import org.broad.igv.feature.NamedFeature;
 import org.broad.igv.feature.genome.Genome;
 import org.broad.igv.feature.genome.fasta.FastaBlockCompressedSequence;
 import org.broad.igv.feature.genome.fasta.FastaIndexedSequence;
+import org.broad.igv.feature.tribble.TribbleIndexNotFoundException;
+import org.broad.igv.track.TribbleFeatureSource;
 import org.broad.igv.util.FileUtils;
 import org.broad.igv.util.ParsingUtils;
 import org.broad.igv.util.ResourceLocator;
@@ -14,8 +23,13 @@ import org.broad.igv.util.ResourceLocator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public class JsonLoader extends GenomeLoader {
+
+    private static Logger log = Logger.getLogger(JsonLoader.class);
 
     private String genomePath;
 
@@ -50,6 +64,7 @@ public class JsonLoader extends GenomeLoader {
                 new FastaIndexedSequence(fastaPath, indexPath);
 
         ArrayList<ResourceLocator> tracks = new ArrayList<>();
+        ArrayList<ResourceLocator> hiddenTracks = new ArrayList<>();
         JsonArray annotations = json.getAsJsonArray("tracks");
         if (annotations == null) {
             annotations = json.getAsJsonArray("annotations");
@@ -60,7 +75,10 @@ public class JsonLoader extends GenomeLoader {
                 String trackPath = obj.get("url").getAsString();
                 JsonElement trackName = obj.get("name");
                 JsonElement trackIndex = obj.get("indexURL");
-                JsonElement indexed = obj.get("indexed");
+                JsonElement indexedElement = obj.get("indexed");
+                JsonElement hiddenElement = obj.get("hidden");
+                boolean hidden = hiddenElement != null && hiddenElement.getAsBoolean();
+                boolean indexed = indexedElement != null && indexedElement.getAsBoolean();
 
                 String trackIndexPath = null;
                 if (trackPath != null) {
@@ -73,8 +91,17 @@ public class JsonLoader extends GenomeLoader {
                 ResourceLocator res = new ResourceLocator(trackPath);
                 if (trackName != null) res.setName(trackName.getAsString());
                 if (trackIndexPath != null) res.setIndexPath(trackIndexPath);
-                if (indexed != null) res.setIndexed(indexed.getAsBoolean());
-                tracks.add(res);
+                if (indexedElement != null) res.setIndexed(indexed);
+
+                if(hidden) {
+                    if(indexed || trackIndex != null) {
+                        log.info("Hidden tracks cannot be indexed.  Ignoring " + trackPath);
+                    } else {
+                        hiddenTracks.add(res);
+                    }
+                } else {
+                    tracks.add(res);
+                }
             });
         }
 
@@ -83,8 +110,27 @@ public class JsonLoader extends GenomeLoader {
         if (aliasURL != null) {
             newGenome.addChrAliases(GenomeLoader.loadChrAliases(aliasURL.getAsString()));
         }
-
+        if(hiddenTracks.size() > 0) {
+            addToFeatureDB(hiddenTracks, newGenome);
+        }
         return newGenome;
+    }
+
+    private void addToFeatureDB(List<ResourceLocator> locators, Genome genome) {
+        for(ResourceLocator locator: locators) {
+            try {
+                FeatureReader featureReader = TribbleFeatureSource.getBasicReader(locator, genome);
+                CloseableTribbleIterator<Feature> iter = featureReader.iterator();
+                while (iter.hasNext()) {
+                    Feature f = iter.next();
+                    if (f instanceof NamedFeature) {
+                        FeatureDB.addFeature((NamedFeature) f, genome);
+                    }
+                }
+            } catch (IOException e) {
+                log.error("Error loading " + locator.getPath());
+            }
+        }
     }
 
 }
