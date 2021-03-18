f67e99633f4fded2f7e724ef9f00edb9e9aa2c64
jrobinso <jrobinso@broadinstitute.org>
2021-03-17 21:47:46 -0700

    Introduce simple pool for SamReader instances -- slight performance gain, a SamReader takes on the order of 100 ms to create.

--- src/main/java/org/broad/igv/sam/reader/BAMReader.java
+++ src/main/java/org/broad/igv/sam/reader/BAMReader.java
@@ -57,66 +57,27 @@ public class BAMReader implements AlignmentReader<PicardAlignment> {
 
     static Logger log = Logger.getLogger(BAMReader.class);
 
-    private final ResourceLocator locator;
-    private final boolean requireIndex;
-
-    SAMFileHeader header;
-    //htsjdk.samtools.SamReader reader;
-    List<String> sequenceNames;
-    private boolean indexed = false; // False until proven otherwise
+    private SAMFileHeader header;
+    private List<String> sequenceNames;
+    private boolean indexed;
     private Map<String, Long> sequenceDictionary;
+    private SamReaderPool readerPool;
 
     public BAMReader(ResourceLocator locator, boolean requireIndex) throws IOException {
-        this.locator = locator;
-        this.requireIndex = requireIndex;
-        SamReader reader = getSamReader();
+        this.indexed = requireIndex;
+        readerPool = new SamReaderPool(locator, requireIndex);
+
+        SamReader reader = readerPool.getReader();
         header = reader.getFileHeader();
+        readerPool.freeReader(reader);
     }
 
     private SamReader getSamReader() throws IOException {
-
-        boolean isLocal = locator.isLocal();
-        final SamReaderFactory factory = SamReaderFactory.makeDefault().
-                referenceSource(new IGVReferenceSource()).
-                validationStringency(ValidationStringency.SILENT);
-        SamInputResource resource;
-
-        if (isLocal) {
-            resource = SamInputResource.of(new File(locator.getPath()));
-        } else {
-            URL url = HttpUtils.createURL(locator.getPath());
-            if (requireIndex) {
-                resource = SamInputResource.of(IGVSeekableStreamFactory.getInstance().getStreamFor(url));
-            } else {
-                resource = SamInputResource.of(new BufferedInputStream(HttpUtils.getInstance().openConnectionStream(url)));
-            }
-        }
-
-        if (requireIndex) {
-
-            String indexPath = getExplicitIndexPath(locator);
-            if (indexPath == null || indexPath.length() == 0) {
-                indexPath = getIndexPath(locator.getPath());
-            }
-
-            indexed = true;
-            if (isLocal) {
-                File indexFile = new File(indexPath);
-                resource = resource.index(indexFile);
-            } else {
-                SeekableStream indexStream = IGVSeekableStreamFactory.getInstance().getStreamFor(HttpUtils.createURL(indexPath));
-                resource = resource.index(indexStream);
-            }
-        }
-
-        return factory.open(resource);
-
+        return readerPool.getReader();
     }
 
     public void close() throws IOException {
-        //    if (reader != null) {
-        //        reader.close();
-        //    }
+        readerPool.close();
     }
 
     public synchronized SAMFileHeader getFileHeader() {
@@ -137,7 +98,6 @@ public class BAMReader implements AlignmentReader<PicardAlignment> {
     public Set<String> getPlatforms() {
         return AlignmentReaderFactory.getPlatforms(getFileHeader());
     }
-
 
     public List<String> getSequenceNames() {
         if (sequenceNames == null) {
@@ -181,7 +141,6 @@ public class BAMReader implements AlignmentReader<PicardAlignment> {
         if (sequenceDictionary != null && !sequenceDictionary.containsKey(sequence)) {
             return EMPTY_ITERATOR;
         } else {
-            CloseableIterator<SAMRecord> iter = null;
             try {
                 SamReader samReader = getSamReader();
                 return new PicardIterator(samReader, sequence, start + 1, end, contained);
@@ -191,170 +150,6 @@ public class BAMReader implements AlignmentReader<PicardAlignment> {
             }
         }
     }
-
-    /**
-     * Fetch an explicitly set index path, either via the ResourceLocator or as a parameter in a URL
-     *
-     * @param locator
-     * @return the index path, or null if no index path is set
-     */
-    private String getExplicitIndexPath(ResourceLocator locator) {
-
-        String p = locator.getPath().toLowerCase();
-        String idx = locator.getIndexPath();
-
-        if (idx == null && (p.startsWith("http://") || p.startsWith("https://"))) {
-            try {
-                URL url = HttpUtils.createURL(locator.getPath());
-                String queryString = url.getQuery();
-                if (queryString != null) {
-                    Map<String, String> parameters = URLUtils.parseQueryString(queryString);
-                    if (parameters.containsKey("index")) {
-                        idx = parameters.get("index");
-
-                    }
-                }
-            } catch (MalformedURLException e) {
-                log.error("Error parsing url: " + locator.getPath());
-            }
-        }
-        return idx;
-    }
-
-    /**
-     * Try to guess the index path.
-     *
-     * @param pathOrURL
-     * @return
-     * @throws IOException
-     */
-    private String getIndexPath(String pathOrURL) throws IOException {
-
-        List<String> pathsTried = new ArrayList<String>();
-
-        String indexPath;
-
-        if (URLUtils.isURL(pathOrURL)) {
-
-            String path = URLUtils.getPath(pathOrURL);
-
-            if (path.endsWith(".bam")) {
-                // Try .bam.bai
-                indexPath = URLUtils.addExtension(pathOrURL, ".bai");
-                pathsTried.add(indexPath);
-                if (HttpUtils.getInstance().resourceAvailable(indexPath)) {
-                    return indexPath;
-                }
-
-                // Try .bai
-                indexPath = URLUtils.replaceExtension(pathOrURL, ".bam", ".bai");
-                pathsTried.add(indexPath);
-                if (HttpUtils.getInstance().resourceAvailable(indexPath)) {
-                    return indexPath;
-                }
-
-                // Try .bam.csi
-                indexPath = URLUtils.addExtension(pathOrURL, ".csi");
-                pathsTried.add(indexPath);
-                if (HttpUtils.getInstance().resourceAvailable(indexPath)) {
-                    return indexPath;
-                }
-
-                // Try .csi
-                indexPath = URLUtils.replaceExtension(pathOrURL, ".bam", ".csi");
-                pathsTried.add(indexPath);
-                if (HttpUtils.getInstance().resourceAvailable(indexPath)) {
-                    return indexPath;
-                }
-            }
-
-            //  cram
-            if (path.endsWith(".cram")) {
-                indexPath = URLUtils.addExtension(pathOrURL, ".crai");
-                if (FileUtils.resourceExists(indexPath)) {
-                    pathsTried.add(indexPath);
-                    return indexPath;
-                } else {
-                    indexPath = pathOrURL.substring(0, pathOrURL.length() - 5) + ".crai";
-                    if (FileUtils.resourceExists(indexPath)) {
-                        return indexPath;
-                    }
-                }
-            }
-
-
-        } else {
-            // Local file
-
-            indexPath = pathOrURL + ".bai";
-
-            if (FileUtils.resourceExists(indexPath)) {
-                return indexPath;
-            }
-
-            if (indexPath.contains(".bam.bai")) {
-                indexPath = indexPath.replaceFirst(".bam.bai", ".bai");
-                pathsTried.add(indexPath);
-                if (FileUtils.resourceExists(indexPath)) {
-                    return indexPath;
-                }
-            } else {
-                indexPath = indexPath.replaceFirst(".bai", ".bam.bai");
-                pathsTried.add(indexPath);
-                if (FileUtils.resourceExists(indexPath)) {
-                    return indexPath;
-                }
-            }
-
-
-            // Try .bam.csi
-            indexPath = pathOrURL + ".csi";
-            pathsTried.add(indexPath);
-            if (FileUtils.resourceExists(indexPath)) {
-                return indexPath;
-            }
-
-            // Try .csi
-            if (pathOrURL.endsWith(".bam")) {
-                indexPath = pathOrURL.substring(0, pathOrURL.length() - 4) + ".csi";
-                pathsTried.add(indexPath);
-                if (FileUtils.resourceExists(indexPath)) {
-                    return indexPath;
-                }
-            }
-
-            if (pathOrURL.endsWith(".cram")) {
-                indexPath = pathOrURL + ".crai";
-                if (FileUtils.resourceExists(indexPath)) {
-                    return indexPath;
-                } else {
-                    indexPath = pathOrURL.substring(0, pathOrURL.length() - 5) + ".crai";
-                    if (FileUtils.resourceExists(indexPath)) {
-                        return indexPath;
-                    }
-                }
-            }
-
-
-        }
-
-        String defaultValue = pathOrURL + (pathOrURL.endsWith(".cram") ? ".crai" : ".bai");
-        indexPath = MessageUtils.showInputDialog(
-                "Index is required, but no index found.  Please enter path to index file:",
-                defaultValue);
-        if (indexPath != null && FileUtils.resourceExists(indexPath)) {
-            return indexPath;
-        }
-
-
-        String msg = "Index file not found.  Tried ";
-        for (String p : pathsTried) {
-            msg += "<br>" + p;
-        }
-        throw new DataLoadException(msg, indexPath);
-
-    }
-
 
     static CloseableIterator<PicardAlignment> EMPTY_ITERATOR = new CloseableIterator<PicardAlignment>() {
         @Override
@@ -374,7 +169,7 @@ public class BAMReader implements AlignmentReader<PicardAlignment> {
     };
 
 
-    static class PicardIterator implements CloseableIterator<PicardAlignment> {
+    class PicardIterator implements CloseableIterator<PicardAlignment> {
 
         private SamReader reader;    // Readers are 1-time use (for this iterator only)
         CloseableIterator<SAMRecord> iterator;
@@ -386,12 +181,7 @@ public class BAMReader implements AlignmentReader<PicardAlignment> {
 
         public void close() {
             iterator.close();
-            try {
-                reader.close();
-            } catch (IOException e) {
-                log.error("Error closing SamReader", e);
-            }
-
+            readerPool.freeReader(this.reader);
         }
 
         public boolean hasNext() {
@@ -407,6 +197,4 @@ public class BAMReader implements AlignmentReader<PicardAlignment> {
         }
 
     }
-
-
 }
