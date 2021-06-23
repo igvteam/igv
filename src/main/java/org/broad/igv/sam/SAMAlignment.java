e7ac0929e4ac2ad1308d44d96994657fdeaf16ee
jrobinso <jrobinso@broadinstitute.org>
2021-06-22 22:51:38 -0400

    Fix NPE bug in "copy read details to clipboard".

--- src/main/java/org/broad/igv/sam/SAMAlignment.java
+++ src/main/java/org/broad/igv/sam/SAMAlignment.java
@@ -611,7 +611,7 @@ public class SAMAlignment implements Alignment {
         }
 
         // Check base modifications
-        if (renderOptions.getColorOption() == AlignmentTrack.ColorOption.BASE_MODIFICATION) {
+        if (renderOptions != null && renderOptions.getColorOption() == AlignmentTrack.ColorOption.BASE_MODIFICATION) {
             for (AlignmentBlock block : this.alignmentBlocks) {
                 if (block.contains((int) position)) {
                     int p = (int) (position - block.getStart()) + block.getBasesOffset();
