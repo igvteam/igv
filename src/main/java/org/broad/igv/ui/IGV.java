igvteam/igv

commit 8a8409b7ad66dc114caceb05e93576ed0a2ce2d3

Refactoring of sample attribute management to fix a number of panel layout bugs.

src/main/java/org/broad/igv/ui/IGV.java
@@ -484,8 +484,7 @@ public class IGV implements IGVEventObserver {
                     List<Map<TrackPanelScrollPane, Integer>> trackPanelAttrs = getTrackPanelAttrs();
                     loadResources(locators);
                     resetPanelHeights(trackPanelAttrs.get(0), trackPanelAttrs.get(1));
-                    showLoadedTrackCount();
-                    IGV.this.getMainPanel().updatePanelDimensions();  // Visible attributes might have changed
+                    showLoadedTrackCount(); // Visible attributes might have changed
                     UIUtilities.invokeAndWaitOnEventThread(() -> {
                         IGV.this.getMainPanel().applicationHeaderPanel.doLayout();  // Forcing this is neccessary if # of attributes change, not sure why
                         IGV.this.getMainPanel().revalidate();
@@ -642,7 +641,6 @@ public class IGV implements IGVEventObserver {
             PreferencesManager.getPreferences().remove(RECENT_SESSIONS);
             PreferencesManager.getPreferences().setRecentSessions(recentSessions);
         }
-
     }
 
     final public void doShowAttributeDisplay(boolean enableAttributeView) {
@@ -657,9 +655,9 @@ public class IGV implements IGVEventObserver {
     }
 
 
-    // TODO -- move all of this attribute stuff out of IGV,  perhaps to
-    // some Attribute helper class.
-
+    /**
+     * Set the attributes to show in the attribute panel for this session.
+     */
     final public void doSelectDisplayableAttribute() {
 
         List<String> allAttributes = AttributeManager.getInstance().getAttributeNames();
@@ -668,16 +666,8 @@ public class IGV implements IGVEventObserver {
         dlg.setVisible(true);
 
         if (!dlg.isCanceled()) {
-            // If any "default" attributes are checked turn off hide default option
-            Set<String> selections = dlg.getSelections();
-            for (String att : AttributeManager.defaultTrackAttributes) {
-                if (selections.contains(att)) {
-                    PreferencesManager.getPreferences().put(SHOW_DEFAULT_TRACK_ATTRIBUTES, true);
-                    break;
-                }
-            }
             IGV.getInstance().getSession().setHiddenAttributes(dlg.getNonSelections());
-            getMainPanel().revalidateTrackPanels();
+            revalidateTrackPanels();
         }
     }
 
@@ -889,6 +879,7 @@ public class IGV implements IGVEventObserver {
             return Cursor.getPredefinedCursor(defaultCursor);
         }
     }
+
     private void subscribeToEvents() {
         IGVEventBus.getInstance().subscribe(ViewChange.class, this);
         IGVEventBus.getInstance().subscribe(InsertionSelectionEvent.class, this);
@@ -1028,6 +1019,7 @@ public class IGV implements IGVEventObserver {
         }
 
         AttributeManager.getInstance().clearAllAttributes();
+        session.clearHiddenAttributes();
         mainFrame.setTitle(sessionPath == null ? UIConstants.APPLICATION_NAME : sessionPath);
         menuBar.resetSessionActions();
 
@@ -1035,7 +1027,6 @@ public class IGV implements IGVEventObserver {
 
         groupByAttribute = null;
 
-        getMainPanel().updatePanelDimensions();
         getMainPanel().revalidateTrackPanels();
     }
 
@@ -1109,9 +1100,9 @@ public class IGV implements IGVEventObserver {
     public boolean loadSessionFromStream(String sessionPath, String locus, InputStream inputStream) throws IOException {
 
         final SessionReader sessionReader;
-        if(sessionPath != null && (sessionPath.endsWith(".session") || sessionPath.endsWith(".session.txt"))) {
+        if (sessionPath != null && (sessionPath.endsWith(".session") || sessionPath.endsWith(".session.txt"))) {
             sessionReader = new UCSCSessionReader(this);
-        } else if(sessionPath != null && (sessionPath.endsWith(".idxsession") || sessionPath.endsWith(".idxsession.txt"))) {
+        } else if (sessionPath != null && (sessionPath.endsWith(".idxsession") || sessionPath.endsWith(".idxsession.txt"))) {
             sessionReader = new IndexAwareSessionReader(this);
         } else {
             sessionReader = new IGVSessionReader(this);
@@ -1150,6 +1141,7 @@ public class IGV implements IGVEventObserver {
             CircularViewUtilities.clearAll();
         }
 
+        revalidateTrackPanels();
         repaint();
         return true;
     }
@@ -1351,7 +1343,7 @@ public class IGV implements IGVEventObserver {
                 track.setAttributeValue(Globals.TRACK_DATA_TYPE_ATTRIBUTE, track.getTrackType().toString());
             }
         }
-
+        revalidateTrackPanels();
         return newTracks;
     }
 
@@ -1743,6 +1735,7 @@ public class IGV implements IGVEventObserver {
             }
             t.unload();
         }
+        revalidateTrackPanels();
     }
 
     /**
@@ -1756,21 +1749,14 @@ public class IGV implements IGVEventObserver {
         TrackPanel panel = PreferencesManager.getPreferences().getAsBoolean(SHOW_SINGLE_TRACK_PANE_KEY) ?
                 getTrackPanel(DATA_PANEL_NAME) : getTrackPanel(FEATURE_PANEL_NAME);
         SequenceTrack newSeqTrack = new SequenceTrack("Reference sequence");
-
         panel.addTrack(newSeqTrack);
+
         if (newGeneTrack != null) {
+            newGeneTrack.setAttributeValue(Globals.TRACK_NAME_ATTRIBUTE, newGeneTrack.getName());
+            newGeneTrack.setAttributeValue(Globals.TRACK_DATA_FILE_ATTRIBUTE, "");
+            newGeneTrack.setAttributeValue(Globals.TRACK_DATA_TYPE_ATTRIBUTE, newGeneTrack.getTrackType().toString());
             panel.addTrack(newGeneTrack);
         }
-
-    }
-
-    public boolean hasGeneTrack() {
-        FeatureTrack geneTrack = GenomeManager.getInstance().getCurrentGenome().getGeneTrack();
-        if (geneTrack == null) return false;
-        for (Track t : getFeatureTracks()) {
-            if (geneTrack == t) return true;
-        }
-        return false;
     }
 
     public boolean hasSequenceTrack() {
@@ -2261,8 +2247,7 @@ public class IGV implements IGVEventObserver {
                         tp.createDataPanels();
                     }
                     contentPane.getCommandBar().setGeneListMode(FrameManager.isGeneListMode());
-                    contentPane.getMainPanel().applicationHeaderPanel.revalidate();
-                    contentPane.getMainPanel().validate();
+                    revalidateTrackPanels();
                     repaint(contentPane.getMainPanel());
                 }
         );

src/main/java/org/broad/igv/feature/genome/Genome.java
@@ -497,7 +497,7 @@ public class Genome {
     }
 
     /**
-     * Return the annotation track associated with this genome.   Can return null
+     * Return the annotation track associated with this genome.  This is a legacy ".genome" artifact, can return null.
      *
      * @return a FeatureTrack, or null
      */

src/main/java/org/broad/igv/feature/genome/GenomeManager.java
@@ -281,6 +281,7 @@ public class GenomeManager {
                 }
             }
         }
+        IGV.getInstance().revalidateTrackPanels();
     }
 
 

src/main/java/org/broad/igv/prefs/IGVPreferences.java
@@ -219,24 +219,10 @@ public class IGVPreferences {
         }
     }
 
-
     public boolean hasExplicitValue(String key) {
         key = key.trim();
         return userPreferences.containsKey(key);
     }
-
-    /**
-     * Get the default value for the specified key.
-     * May be null.
-     *
-     * @param key
-     * @return
-     */
-    public String getDefaultValue(String key) {
-        key = key.trim();
-        return parent == null ? get(key) : parent.get(key);
-    }
-
 
     public void addOverrides(Map<String, String> newPrefs) {
         overrideKeys.addAll(newPrefs.keySet());
@@ -252,7 +238,6 @@ public class IGVPreferences {
             return false;
         }
     }
-
 
     /**
      * Update any cached values with the new key/value pair
@@ -309,6 +294,7 @@ public class IGVPreferences {
         checkForProxyChanges(updatedPrefs);
         checkForAlignmentChanges(updatedPrefs);
         checkForCommandListenerChanges(updatedPrefs);
+        checkForAttributePanelChanges(updatedPrefs);
         checkForCircViewChanges(updatedPrefs);
         IGVEventBus.getInstance().post(new PreferencesChangeEvent());
 
@@ -370,10 +356,16 @@ public class IGVPreferences {
         }
     }
 
+    private void checkForAttributePanelChanges(Map<String, String> updatedPreferenceMap) {
+        if (updatedPreferenceMap.containsKey(SHOW_ATTRIBUTE_VIEWS_KEY) || updatedPreferenceMap.containsKey(SHOW_DEFAULT_TRACK_ATTRIBUTES)) {
+           if(IGV.hasInstance()) {
+               IGV.getInstance().revalidateTrackPanels();
+           }
+        }
+    }
+
     /**
      * Enabling circ view requires port listener
-     *
-     * @param updatedPreferenceMap
      */
     private void checkForCircViewChanges(Map<String, String> updatedPreferenceMap) {
         if (updatedPreferenceMap.containsKey(CIRC_VIEW_ENABLED) &&

src/main/java/org/broad/igv/session/IGVSessionReader.java
@@ -270,7 +270,6 @@ public class IGVSessionReader implements SessionReader {
             }
         }
 
-        session.setUcscId(getAttribute(element, SessionAttribute.UCSC_ID));
         session.setLocus(getAttribute(element, SessionAttribute.LOCUS));
         session.setGroupTracksBy(getAttribute(element, SessionAttribute.GROUP_TRACKS_BY));
 

src/main/java/org/broad/igv/session/Session.java
@@ -25,11 +25,15 @@
 
 package org.broad.igv.session;
 
-import org.broad.igv.logging.*;
 import org.broad.igv.Globals;
+import org.broad.igv.event.IGVEventBus;
+import org.broad.igv.event.IGVEventObserver;
+import org.broad.igv.event.ViewChange;
 import org.broad.igv.feature.Range;
 import org.broad.igv.feature.RegionOfInterest;
 import org.broad.igv.lists.GeneList;
+import org.broad.igv.logging.LogManager;
+import org.broad.igv.logging.Logger;
 import org.broad.igv.prefs.IGVPreferences;
 import org.broad.igv.prefs.PreferencesManager;
 import org.broad.igv.renderer.ContinuousColorScale;
@@ -38,9 +42,6 @@ import org.broad.igv.track.AttributeManager;
 import org.broad.igv.track.TrackType;
 import org.broad.igv.ui.IGV;
 import org.broad.igv.ui.TrackFilter;
-import org.broad.igv.event.IGVEventBus;
-import org.broad.igv.event.IGVEventObserver;
-import org.broad.igv.event.ViewChange;
 import org.broad.igv.ui.panel.FrameManager;
 import org.broad.igv.ui.panel.ReferenceFrame;
 import org.broad.igv.util.ObservableForObject;
@@ -55,16 +56,6 @@ import static org.broad.igv.prefs.Constants.*;
 public class Session implements IGVEventObserver {
 
     private static Logger log = LogManager.getLogger(Session.class);
-    private String ucscId;
-
-    public void setUcscId(String ucscId) {
-        this.ucscId = ucscId;
-    }
-
-    public String getUcscId() {
-        return ucscId;
-    }
-
 
     //This doesn't mean genelist or not, the same way it does in FrameManager
     public enum GeneListMode {
@@ -135,11 +126,9 @@ public class Session implements IGVEventObserver {
         }
     }
 
-
     public void clearDividerLocations() {
         dividerFractions = null;
     }
-
 
     public void setDividerFractions(double[] divs) {
         this.dividerFractions = divs;
@@ -431,24 +420,31 @@ public class Session implements IGVEventObserver {
         return this.nextAutoscaleGroup++;
     }
 
+    /**
+     * Return a set containing names of attributes explicitly marked hidden.  If no attributes have been explicitly
+     * marked return the default set.
+     *
+     * @return
+     */
     public Set<String> getHiddenAttributes() {
-
-        Set<String> extendedHiddenAttributes = new HashSet<String>();
-        if (hiddenAttributes != null) {
-            extendedHiddenAttributes.addAll(hiddenAttributes);
+        if (hiddenAttributes == null || hiddenAttributes.isEmpty()) {
+            return (PreferencesManager.getPreferences().getAsBoolean(SHOW_DEFAULT_TRACK_ATTRIBUTES))  ?
+                    Collections.emptySet() :
+                    new HashSet<>(AttributeManager.defaultTrackAttributes);
+        } else {
+            return hiddenAttributes;
         }
-        if (!PreferencesManager.getPreferences().getAsBoolean(SHOW_DEFAULT_TRACK_ATTRIBUTES)) {
-
-            extendedHiddenAttributes.addAll(AttributeManager.defaultTrackAttributes);
-        }
-
-        return extendedHiddenAttributes;
     }
 
     public void setHiddenAttributes(Set<String> attributes) {
         this.hiddenAttributes = attributes;
 
     }
+
+    public void clearHiddenAttributes() {
+        hiddenAttributes = null;
+    }
+
 
     public boolean isRemoveEmptyPanels() {
         return removeEmptyPanels;

src/main/java/org/broad/igv/session/SessionWriter.java
@@ -148,9 +148,6 @@ public class SessionWriter {
                 globalElement.setAttribute("removeEmptyTracks", "true");
             }
 
-            globalElement.setAttribute(SessionAttribute.HAS_GENE_TRACK, "" + IGV.getInstance().hasGeneTrack());
-            globalElement.setAttribute(SessionAttribute.HAS_SEQ_TRACK, "" + IGV.getInstance().hasSequenceTrack());
-
             // Resource Files
             writeResources(outputFile, globalElement, document);
 

src/main/java/org/broad/igv/track/AttributeManager.java
@@ -31,9 +31,10 @@
 package org.broad.igv.track;
 
 import htsjdk.tribble.readers.AsciiLineReader;
-import org.broad.igv.logging.*;
 import org.broad.igv.Globals;
 import org.broad.igv.exceptions.DataLoadException;
+import org.broad.igv.logging.LogManager;
+import org.broad.igv.logging.Logger;
 import org.broad.igv.prefs.Constants;
 import org.broad.igv.prefs.PreferencesManager;
 import org.broad.igv.renderer.AbstractColorScale;
@@ -44,7 +45,6 @@ import org.broad.igv.ui.color.ColorPalette;
 import org.broad.igv.ui.color.ColorTable;
 import org.broad.igv.ui.color.ColorUtilities;
 import org.broad.igv.ui.color.PaletteColorTable;
-import org.broad.igv.ui.util.MessageUtils;
 import org.broad.igv.util.FileUtils;
 import org.broad.igv.util.ParsingUtils;
 import org.broad.igv.util.ResourceLocator;
@@ -56,8 +56,8 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.*;
 import java.util.List;
+import java.util.*;
 
 /**
  * @author jrobinso
@@ -123,14 +123,9 @@ public class AttributeManager {
 
     Map<String, ColorTable> colorTables = new HashMap<String, ColorTable>();
 
-    Map<String, Integer> colorCounter = new HashMap();
 
     private AttributeManager() {
         propertyChangeSupport = new PropertyChangeSupport(this);
-        //hiddenAttributes.add("NAME");
-        //hiddenAttributes.add("DATA FILE");
-        //hiddenAttributes.add("DATA TYPE");
-
     }
 
     static synchronized public AttributeManager getInstance() {

commit ea228c3136dd6aeee6fe624ce55402deced17782

Fix bug introduced with commit 8a8409b7ad66dc114caceb05e93576ed0a2ce2d3.

src/main/java/org/broad/igv/ui/IGV.java
@@ -484,7 +484,8 @@ public class IGV implements IGVEventObserver {
                     List<Map<TrackPanelScrollPane, Integer>> trackPanelAttrs = getTrackPanelAttrs();
                     loadResources(locators);
                     resetPanelHeights(trackPanelAttrs.get(0), trackPanelAttrs.get(1));
-                    showLoadedTrackCount(); // Visible attributes might have changed
+                    showLoadedTrackCount();
+                    IGV.this.getMainPanel().updatePanelDimensions();  // Visible attributes might have changed
                     UIUtilities.invokeAndWaitOnEventThread(() -> {
                         IGV.this.getMainPanel().applicationHeaderPanel.doLayout();  // Forcing this is neccessary if # of attributes change, not sure why
                         IGV.this.getMainPanel().revalidate();
@@ -667,7 +668,7 @@ public class IGV implements IGVEventObserver {
 
         if (!dlg.isCanceled()) {
             IGV.getInstance().getSession().setHiddenAttributes(dlg.getNonSelections());
-            revalidateTrackPanels();
+            getMainPanel().revalidateTrackPanels();
         }
     }
 
@@ -1027,6 +1028,7 @@ public class IGV implements IGVEventObserver {
 
         groupByAttribute = null;
 
+        getMainPanel().updatePanelDimensions();
         getMainPanel().revalidateTrackPanels();
     }
 
@@ -1100,9 +1102,9 @@ public class IGV implements IGVEventObserver {
     public boolean loadSessionFromStream(String sessionPath, String locus, InputStream inputStream) throws IOException {
 
         final SessionReader sessionReader;
-        if (sessionPath != null && (sessionPath.endsWith(".session") || sessionPath.endsWith(".session.txt"))) {
+        if(sessionPath != null && (sessionPath.endsWith(".session") || sessionPath.endsWith(".session.txt"))) {
             sessionReader = new UCSCSessionReader(this);
-        } else if (sessionPath != null && (sessionPath.endsWith(".idxsession") || sessionPath.endsWith(".idxsession.txt"))) {
+        } else if(sessionPath != null && (sessionPath.endsWith(".idxsession") || sessionPath.endsWith(".idxsession.txt"))) {
             sessionReader = new IndexAwareSessionReader(this);
         } else {
             sessionReader = new IGVSessionReader(this);
@@ -1141,7 +1143,7 @@ public class IGV implements IGVEventObserver {
             CircularViewUtilities.clearAll();
         }
 
-        revalidateTrackPanels();
+        //revalidateTrackPanels();
         repaint();
         return true;
     }
@@ -1343,7 +1345,7 @@ public class IGV implements IGVEventObserver {
                 track.setAttributeValue(Globals.TRACK_DATA_TYPE_ATTRIBUTE, track.getTrackType().toString());
             }
         }
-        revalidateTrackPanels();
+        //revalidateTrackPanels();
         return newTracks;
     }
 
@@ -2247,7 +2249,8 @@ public class IGV implements IGVEventObserver {
                         tp.createDataPanels();
                     }
                     contentPane.getCommandBar().setGeneListMode(FrameManager.isGeneListMode());
-                    revalidateTrackPanels();
+                    contentPane.getMainPanel().applicationHeaderPanel.revalidate();
+                    contentPane.getMainPanel().validate();
                     repaint(contentPane.getMainPanel());
                 }
         );