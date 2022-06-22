9491e8bcc1b6a2290e900c4002d6a3016cbbdd65
M183305 <alaparthi.tejaswi@mayo.edu>
2022-06-22 09:58:26 -0500

    Add config json

--- src/main/java/org/broad/igv/google/OAuthUtils.java
+++ src/main/java/org/broad/igv/google/OAuthUtils.java
@@ -109,7 +109,7 @@ public class OAuthUtils {
         }
 
         // Local config takes precendence, overriding URL provisioned and Broad's default oauth-config.json.gz
-        String oauthConfig = DirectoryManager.getIgvDirectory() + "/oauth-config-2.13.0.MAYO.json";
+        String oauthConfig = DirectoryManager.getIgvDirectory() + "/oauth-config-2.13.0.json";
         if ((new File(oauthConfig)).exists()) {
             try {
                 log.debug("Loading Oauth properties from: " + oauthConfig);
