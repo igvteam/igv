f13764419d36d3268676a58d6b74b10f81f38cee
Louis Bergelson <louisb@broadinstitute.org>
2022-08-15 18:09:16 -0400

    fix incorrect detection of tooltip region for variant track name

--- src/main/java/org/broad/igv/variant/VariantTrack.java
+++ src/main/java/org/broad/igv/variant/VariantTrack.java
@@ -900,7 +900,7 @@ public class VariantTrack extends FeatureTrack implements IGVEventObserver {
     }
 
     public String getNameValueString(int y) {
-        if (y < top + getVariantBandHeight()) {
+        if (y < top + getVariantsHeight()) {
             return super.getNameValueString(y);
         } else {
             String sample = getSampleAtPosition(y);
