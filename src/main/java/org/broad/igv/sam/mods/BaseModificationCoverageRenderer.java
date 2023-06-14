7aad51b92650c02a3b54c0f248c405466235f354
Chris Saunders <csaunders@pacificbiosciences.com>
2023-06-14 16:14:22 -0700

    Consolidate BaseModificationCoverageRenderer.draw
    Consolidate general and 6mA cases down to one method

--- src/main/java/org/broad/igv/sam/mods/BaseModificationCoverageRenderer.java
+++ src/main/java/org/broad/igv/sam/mods/BaseModificationCoverageRenderer.java
@@ -32,10 +32,10 @@ public class BaseModificationCoverageRenderer {
                 draw5MC(context, pX, pBottom, dX, barHeight, pos, alignmentCounts, true);
                 break;
             case BASE_MODIFICATION_6MA:
-                draw6MA(context, pX, pBottom, dX, barHeight, pos, alignmentCounts);
+                draw(context, pX, pBottom, dX, barHeight, pos, alignmentCounts, true);
                 break;
             default:
-                draw(context, pX, pBottom, dX, barHeight, pos, alignmentCounts);
+                draw(context, pX, pBottom, dX, barHeight, pos, alignmentCounts, false);
         }
     }
 
@@ -46,7 +46,8 @@ public class BaseModificationCoverageRenderer {
                              int dX,
                              int barHeight,
                              int pos,
-                             AlignmentCounts alignmentCounts) {
+                             AlignmentCounts alignmentCounts,
+                             boolean onlyDraw6mA) {
 
         BaseModificationCounts modificationCounts = alignmentCounts.getModifiedBaseCounts();
 
@@ -56,6 +57,12 @@ public class BaseModificationCoverageRenderer {
 
             for (BaseModificationCounts.Key key : modificationCounts.getAllModifications()) {
 
+                String modification = key.getModification();
+                if (onlyDraw6mA) {
+                    if (key.getCanonicalBase() != 'A' && key.getCanonicalBase() != 'T') continue;
+                    if (!modification.equals("a")) continue;
+                }
+
                 // The number of modification calls, some of which might have likelihood of zero
                 int modificationCount = modificationCounts.getCount(pos, key);
 
@@ -63,8 +70,6 @@ public class BaseModificationCoverageRenderer {
 
                     byte base = (byte) key.getBase();
                     byte complement = SequenceUtil.complement(base);
-                    char modStrand = key.getStrand();
-                    String modification = key.getModification();
 
                     // Count of bases at this location that could potentially be modified, accounting for strand
                     int baseCount = alignmentCounts.getPosCount(pos, base) + alignmentCounts.getNegCount(pos, complement);
@@ -155,82 +160,6 @@ public class BaseModificationCoverageRenderer {
                 Arrays.sort(orderedMods, (o1, o2) -> -1 * o1.compareTo(o2));
                 for (String m : orderedMods) {
                     Color mColor = BaseModificationColors.getModColor(m, (byte) 255, ColorOption.BASE_MODIFICATION_5MC);
-                    int mModHeight = (int) Math.round(((likelihoodSums.get(m)) / t) * calledBarHeight);
-                    if (mModHeight > 0) {
-                        baseY -= mModHeight;
-                        graphics.setColor(mColor);
-                        graphics.fillRect(pX, baseY, dX, mModHeight);
-                    }
-                }
-            }
-        }
-    }
-
-    private static void draw6MA(RenderContext context,
-                                int pX,
-                                int pBottom,
-                                int dX,
-                                int barHeight,
-                                int pos,
-                                AlignmentCounts alignmentCounts) {
-
-        BaseModificationCounts modificationCounts = alignmentCounts.getModifiedBaseCounts();
-
-        if (modificationCounts != null) {
-
-            final byte base = (byte) 'A';
-            final byte complement = (byte) 'T';
-
-            Map<String, Integer> likelihoodSums = new HashMap<>();
-            Map<String, Integer> modCounts = new HashMap<>();
-            for (BaseModificationCounts.Key key : modificationCounts.getAllModifications()) {
-
-                if (key.getCanonicalBase() != 'A' && key.getCanonicalBase() != 'T') continue;
-                if (key.getModification().equals("a")) {
-                    String mod = key.getModification();
-                    final int count = modificationCounts.getCount(pos, key);
-                    if (count > 0) {
-                        modCounts.put(mod, count);
-                        final int likelhoodSum = modificationCounts.getLikelhoodSum(pos, key);
-                        likelihoodSums.put(mod, likelhoodSum);
-                    }
-                }
-            }
-
-            if (likelihoodSums.size() > 0) {
-
-                // Count of bases at this location that could potentially be modified
-                double modifiableBaseCount = alignmentCounts.getPosCount(pos, base) + alignmentCounts.getNegCount(pos, complement);
-
-                // Compute "snp factor", ratio of count of base calls that could be modfied (on either strand) to
-                // total count. This is normally close to 1, but can be less due non CG bases at this location (e.g. snps)
-                double cgCount = alignmentCounts.getCount(pos, base) + alignmentCounts.getCount(pos, complement);
-                double snpFactor = cgCount / alignmentCounts.getTotalCount(pos);
-
-                double calledBarHeight = snpFactor * barHeight;
-                double t = modifiableBaseCount * 255;   // If all bases are called this is the total sum of all likelihoods, including "no mod" likelihood
-
-                // Likelihood of no modification.  It is assumed that the likelihood of no modification == (1 - sum(likelihood))
-                int c = Collections.max(modCounts.values());
-                double noModProb = c * 255;
-                for (String m : modCounts.keySet()) {
-                    if (likelihoodSums.containsKey(m)) {
-                        noModProb -= likelihoodSums.get(m);
-                    }
-                }
-
-                // Draw "no mod" bar
-                int noModHeight = (int) Math.round((noModProb / t) * calledBarHeight);
-                int baseY = pBottom - noModHeight;
-                Graphics2D graphics = context.getGraphics();
-                graphics.setColor(BaseModificationColors.noModColor5MC);
-                graphics.fillRect(pX, baseY, dX, noModHeight);
-
-                // Loop through modifications drawing bar for each
-                String[] orderedMods = likelihoodSums.keySet().toArray(new String[0]);
-                Arrays.sort(orderedMods, (o1, o2) -> -1 * o1.compareTo(o2));
-                for (String m : orderedMods) {
-                    Color mColor = BaseModificationColors.getModColor(m, (byte) 255, ColorOption.BASE_MODIFICATION_6MA);
                     int mModHeight = (int) Math.round(((likelihoodSums.get(m)) / t) * calledBarHeight);
                     if (mModHeight > 0) {
                         baseY -= mModHeight;
