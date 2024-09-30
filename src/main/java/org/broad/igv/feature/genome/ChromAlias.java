887bc9a5ebb13e4c62d37d656f106458129866c4
jrobinso <933148+jrobinso@users.noreply.github.com>
2024-09-30 12:05:10 -0700

    fix for chrom alias defaults

--- src/main/java/org/broad/igv/feature/genome/ChromAlias.java
+++ src/main/java/org/broad/igv/feature/genome/ChromAlias.java
@@ -5,34 +5,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ChromAlias {
-    
+
     private String chr;
+
+    /**
+     * Map of name set -> alias
+     */
     private Map<String, String> aliases;
 
     public ChromAlias(String chr) {
         this.chr = chr;
         this.aliases = new HashMap<>();
     }
-    
+
     public String getChr() {
         return chr;
     }
-    
+
     public void put(String nameSet, String alias) {
         aliases.put(nameSet, alias);
     }
+
     public String get(String nameSet) {
         return aliases.get(nameSet);
     }
-    
+
     public boolean containsKey(String nameSet) {
         return aliases.containsKey(nameSet);
     }
-    
+
     public Collection<String> values() {
         return aliases.values();
     }
-    
-    
-    
+
+
 }
