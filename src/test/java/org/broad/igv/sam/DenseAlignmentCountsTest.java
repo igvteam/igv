848a0d2a7e05ebab81e638207a9dfe73bdae3411
Jim Robinson <933148+jrobinso@users.noreply.github.com>
2025-06-24 08:38:41 -0700

    Count non-ACTG read base characters in coverage track (ambiguity code… (#1707)
    * Count non-ACTG read base characters in coverage track (ambiguity codes, '=' character).

--- src/test/java/org/broad/igv/sam/DenseAlignmentCountsTest.java
+++ src/test/java/org/broad/igv/sam/DenseAlignmentCountsTest.java
@@ -26,7 +26,12 @@
 package org.broad.igv.sam;
 
 import org.broad.igv.AbstractHeadlessTest;
+import org.broad.igv.sam.reader.AlignmentReader;
+import org.broad.igv.sam.reader.AlignmentReaderFactory;
+import org.broad.igv.util.TestUtils;
 import org.junit.Test;
+import java.util.Iterator;
+import static org.junit.Assert.*;
 
 /**
  * @author jacob
@@ -56,14 +61,33 @@ public class DenseAlignmentCountsTest extends AbstractHeadlessTest {
         tstGetMaxCount(0, fullIntervals, extraLength);
     }
 
-    private void tstGetMaxCount(int start, int fullIntervals, int extraLength){
-        int mci = DenseAlignmentCounts.MAX_COUNT_INTERVAL;
-        int end = start + fullIntervals*mci + extraLength;
+    @Test
+    public void testEqBase() throws Exception {
+        String chr = "chr1";
+        int start = 59300;
+        int end = 59400;
+        String path = TestUtils.DATA_DIR + "bam/squeeze.sam";
         DenseAlignmentCounts daCounts = new DenseAlignmentCounts(start, end, null);
 
-        int[] queryStarts = {start, start + 50, start + mci, start + mci + 50, start,           start, start, start + 10};
-        int[] queryLengths ={50,    50,         50,          50,               (int) (mci*1.5), 2*mci, (int)(2.5*mci), end-start + 10};
-        for(int ii=0; ii < queryStarts.length; ii++){
+        AlignmentReader reader = AlignmentReaderFactory.getReader(path, false);
+        Iterator<Alignment> iter = reader.iterator();
+        while (iter.hasNext()) {
+            Alignment al = iter.next();
+            daCounts.incCounts(al);
+        }
+        int count = daCounts.getCount(59303, (byte) '=');
+        assertEquals(3, count);
+
+    }
+
+    private void tstGetMaxCount(int start, int fullIntervals, int extraLength) {
+        int mci = DenseAlignmentCounts.MAX_COUNT_INTERVAL;
+        int end = start + fullIntervals * mci + extraLength;
+        DenseAlignmentCounts daCounts = new DenseAlignmentCounts(start, end, null);
+
+        int[] queryStarts = {start, start + 50, start + mci, start + mci + 50, start, start, start, start + 10};
+        int[] queryLengths = {50, 50, 50, 50, (int) (mci * 1.5), 2 * mci, (int) (2.5 * mci), end - start + 10};
+        for (int ii = 0; ii < queryStarts.length; ii++) {
             int queryStart = queryStarts[ii];
             int queryEnd = queryStart + queryLengths[ii];
             daCounts.getMaxCount(start, queryEnd);
