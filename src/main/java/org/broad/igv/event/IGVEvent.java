96641067ea81e358f457dcb36dbcc324dc6d9d8e
jrobinso <933148+jrobinso@users.noreply.github.com>
2025-10-17 18:57:57 -0700

    git rid of modules.  This is possible?

--- src/main/java/org/broad/igv/event/IGVEvent.java
+++ src/main/java/org/broad/igv/event/IGVEvent.java
@@ -5,9 +5,6 @@ import org.broad.igv.prefs.PreferencesChangeEvent;
 import org.broad.igv.sam.InsertionSelectionEvent;
 import org.broad.igv.ui.panel.FrameManager;
 
-public sealed interface IGVEvent
-        permits AlignmentTrackEvent, DataLoadedEvent, GenomeChangeEvent, GenomeResetEvent, RefreshEvent, StopEvent,
-        TrackFilterEvent, TrackGroupEvent, ViewChange, OAuthProvider.AuthStateEvent, PreferencesChangeEvent,
-        InsertionSelectionEvent, FrameManager.ChangeEvent {
+public interface IGVEvent {
 
 }
