4928dba46471012908c677dd9c713525c633fef5
Jim Robinson <933148+jrobinso@users.noreply.github.com>
2026-06-07 15:19:39 -0700

    Update igv-launcher.bat for windows

--- scripts/igv-launcher.bat
+++ scripts/igv-launcher.bat
@@ -1,20 +1,37 @@
 setlocal
+::Get the current batch file's short path
+for %%x in (%0) do set BatchPath=%%~dpsx
+for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
 
-if exist jdk-21 (
+if exist %BatchPath%\jdk-21 (
   echo "Using bundled JDK."
-  set JAVA_HOME=jdk-21
-  set JAVA_CMD=jdk-21\bin\javaw
+  set JAVA_HOME=%BatchPath%\jdk-21
+  set JAVA_CMD=%BatchPath%\jdk-21\bin\javaw
 ) else (
   echo "Using system JDK."
   set JAVA_CMD=java
 )
 
 ::-Xmx8g indicates 8 gb of memory.
-::To adjust this (or other Java options), edit the "%USERPROFILE%\.igv\java_arguments" 
-::file.  For more info, see the README at 
-::https://raw.githubusercontent.com/igvteam/igv/master/scripts/readme.txt 
+::To adjust this (or other Java options), edit the "%USERPROFILE%\.igv\java_arguments"
+::file.  For more info, see the README at
+::https://raw.githubusercontent.com/igvteam/igv/master/scripts/readme.txt
+
+set CP=%BatchPath%lib\*
+
 if exist "%USERPROFILE%\.igv\java_arguments" (
-  start %JAVA_CMD% -showversion --module-path=lib -Xmx8g -Dproduction=true @igv.args -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true -Djava.net.useSystemProxies=true @"%USERPROFILE%\.igv\java_arguments" --module=org.igv/org.igv.ui.Main
+    %JAVA_CMD% -Xmx8g ^
+        @%BatchPath%igv.args ^
+        -Dsamjdk.snappy.disable=true ^
+        -Djava.net.preferIPv4Stack=true ^
+        -Djava.net.useSystemProxies=true ^
+        @"%USERPROFILE%\.igv\java_arguments" ^
+        -cp "%CP%" org.igv.ui.Main %*
 ) else (
-  start %JAVA_CMD% -showversion --module-path=lib -Xmx8g -Dproduction=true @igv.args -Djava.net.preferIPv4Stack=true -Dsun.java2d.noddraw=true -Djava.net.useSystemProxies=true --module=org.igv/org.igv.ui.Main
+    %JAVA_CMD% -Xmx8g ^
+        @%BatchPath%igv.args ^
+        -Dsamjdk.snappy.disable=true ^
+        -Djava.net.preferIPv4Stack=true ^
+        -Djava.net.useSystemProxies=true ^
+        -cp "%CP%" org.igv.ui.Main %*
 )
